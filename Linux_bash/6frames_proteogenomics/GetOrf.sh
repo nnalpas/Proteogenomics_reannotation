3e3fecd04e217b7dfb23716bc98ecf0079c2f33a
nnalpas <kxmna01@uni-tuebingen.de>
2017-01-10 18:29:28 +0100

    Script to get the ORF from genomic fasta file (as nucleotide or aa sequence)

---
+++ Linux_bash/6frames_proteogenomics/GetOrf.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Script to get the ORF from sequence file
+
+# Time scripts starts
+echo "$0"
+echo "Start $(date +"%T %d-%m-%Y")."
+
+# Check for required command line arguments
+if [ $# -lt 6 ]; then
+	echo "Usage: $0 <FastaFile>"
+	exit 1
+fi
+
+# Define variables
+WKDIR=$1
+echo "Working directory: $WKDIR"
+FIND=$2
+TABLE=$3
+MINSIZE=$4
+CIRCULAR=$5
+SEQ="${@:6}"
+
+
+# Create the output folder
+if [ ! -d ${WKDIR} ] ; then
+	mkdir ${WKDIR}
+fi
+
+# Loop through all submitted fasta
+for files in ${SEQ[@]}; do
+	outfile=`basename $files`
+	$HOME/Software/EMBOSS/bin/getorf -sequence ${files} -outseq ${WKDIR}/Find${FIND}_${outfile} -find=${FIND} -table=${TABLE} -minsize=${MINSIZE} -circular=${CIRCULAR}
+done
+
+# Time scripts ends
+echo "Completed $(date +"%T %d-%m-%Y")."
