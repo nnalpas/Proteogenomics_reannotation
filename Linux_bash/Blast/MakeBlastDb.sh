01323cb13b562215738b434f34789f98b22f996a
nnalpas <kxmna01@uni-tuebingen.de>
2017-01-11 18:03:02 +0100

    Modify scripts to allow for additional parameters submission during blast database creation (such as input type and DB type)

--- Linux_bash/Blast/MakeBlastDb.sh
+++ Linux_bash/Blast/MakeBlastDb.sh
@@ -7,19 +7,21 @@ echo "$0"
 echo "Start $(date +"%T %d-%m-%Y")."
 
 # Check for required command line arguments
-if [ $# -lt 1 ]; then
+if [ $# -lt 3 ]; then
 	echo "Usage: $0 <FastaFile>"
 	exit 1
 fi
 
 # Define variables
-FASTA="${@:1}"
+INPUTTYPE=$1
+DBTYPE=$2
+FASTA="${@:3}"
 echo "Working directory: $WKDIR"
 
-# Loop throough all submitted fasta
+# Loop through all submitted fasta
 for fasta in ${FASTA[@]}; do
 	title=`basename $fasta | perl -p -e 's/.fasta$//'`
-	makeblastdb -in ${fasta} -input_type 'fasta' -title ${title} -parse_seqids -dbtype 'prot'
+	makeblastdb -in ${fasta} -input_type ${INPUTTYPE} -title ${title} -parse_seqids -dbtype ${DBTYPE}
 done
 
 # Time scripts ends
