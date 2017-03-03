3f9a211a1f4e6efd9a015d902fe338c5bda6914c
nnalpas <nalpas.nicolas@gmail.com>
2017-03-03 16:50:26 +0100

    Update to use the BinAC batch job manager

--- Linux_bash/Blast/Uniprot_database.sh
+++ Linux_bash/Blast/Uniprot_database.sh
@@ -4,16 +4,15 @@
 # Script that creates a blast database from Uniprot complete entries
 
 # Go to workspace
-WKDIR="~/work/BlastDB"
-mkdir ${WKDIR}
+cd ${PBS_O_WORKDIR}
 
 # Download Uniprot entries (swissprot and trembl)
-wget "ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/complete/uniprot_*.fasta.gz"
+#wget "ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/complete/uniprot_*.fasta.gz"
 
 # To get a single database make sure to compile entries
-gunzip -c uniprot_*.fasta.gz >> uniprot_sprot_trembl.fasta
+#gunzip -c uniprot_*.fasta.gz >> uniprot_sprot_trembl.fasta
  
 # Create the blast database
-MakeBlastDb.sh "fasta" "prot" 131567 "${WKDIR}/uniprot_sprot_trembl.fasta" >> ${WKDIR}/MakeBlastDb.log 2>&1
+${PBS_O_HOME}/bin/MakeBlastDb.sh "fasta" "prot" 131567 "${PBS_O_INITDIR}/uniprot_sprot_trembl.fasta" >> ./MakeBlastDb_{PBS_JOBID}.log
 
 
