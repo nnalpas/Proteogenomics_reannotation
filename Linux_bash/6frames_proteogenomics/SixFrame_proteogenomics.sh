3110b2a4e5549b63e10cbfbb6ae2c94b5f911c55
nnalpas <nalpas.nicolas@gmail.com>
2017-03-04 21:57:47 +0100

    Replace some variables such as HOME and THREADS to be compatible with BinAC server

--- Linux_bash/6frames_proteogenomics/SixFrame_proteogenomics.sh
+++ Linux_bash/6frames_proteogenomics/SixFrame_proteogenomics.sh
@@ -8,18 +8,32 @@
 # Parameters and logs set-up #
 ##############################
 
-# Check whether a parameter file was provided
-if [ $# -ne 1 ]; then
-	echo "Usage: $0 <ParametersPath>"
-	exit 1
+# Check if $SCRIPT_FLAGS is set
+if [ -n "${SCRIPT_FLAGS}" ] ; then
+
+	# But if positional parameters are already present
+	# we are going to ignore $SCRIPT_FLAGS
+	if [ -z "${*}" ] ; then
+		set -- ${SCRIPT_FLAGS}
+	fi
+
 fi
 
+# Check whether a parameter file was provided
+#if [ $# -ne 1 ]; then
+#	echo "Usage: $0 <ParametersPath>"
+#	exit 1
+#fi
+
 # Load all parameters
-Param=$1
-source $Param
+source ${SCRIPT_FLAGS}
+export OMP_NUM_THREADS=${MOAB_PROCCOUNT}
+
+# Go to workspace
+cd ${PBS_O_WORKDIR}
 
 # Create project directory
-ProjDir=${MainDir}/${ProjectName}
+ProjDir=${PBS_O_INITDIR}/${ProjectName}
 
 # Create the log folder
 DateStart=$(date -I)
@@ -27,7 +41,7 @@ LogDir=${ProjDir}/Log/${DateStart}
 mkdir -p $LogDir
 
 # Copy the parameter file to log
-cp $Param ${LogDir}/Parameters.txt
+cp ${SCRIPT_FLAGS} ${LogDir}/Parameters_${DateStart}.txt
 
 
 
@@ -38,8 +52,8 @@ cp $Param ${LogDir}/Parameters.txt
 # Check whether to get the ORFs from genomic sequence
 if [ $GetOrf == 1 ]; then
 
-    ${HOME}/bin/GetOrf.sh ${ProjDir}/Nuc_translation 0 ${TABLE} ${MINSIZE} ${CIRCULAR} ${GENOME} > ${LogDir}/GetOrf.log 2>&1
-    ${HOME}/bin/GetOrf.sh ${ProjDir}/Nuc_translation 2 ${TABLE} ${MINSIZE} ${CIRCULAR} ${GENOME} >> ${LogDir}/GetOrf.log 2>&1
+    ${PBS_O_HOME}/bin/GetOrf.sh ${ProjDir}/Nuc_translation 0 ${TABLE} ${MINSIZE} ${CIRCULAR} ${GENOME} > ${LogDir}/GetOrf.log 2>&1
+    ${PBS_O_HOME}/bin/GetOrf.sh ${ProjDir}/Nuc_translation 2 ${TABLE} ${MINSIZE} ${CIRCULAR} ${GENOME} >> ${LogDir}/GetOrf.log 2>&1
     
 fi
 
@@ -56,7 +70,7 @@ SIXFRAMEGENE=`echo $GENOME | perl -p -e 's/^(.*\\/)(.*)\\.fasta/$1Find2_$2_FIXED
 # Check whether to get the novel ORFs from MaxQuant seach results
 if [ $GetNovelEntries == 1 ]; then
 
-    ${HOME}/bin/GetNovelEntries.R ${ProjDir}/MaxQuant_txt "." ${UNIREFPROT} ${SIXFRAMEPROT} > ${LogDir}/GetNovelEntries.log 2>&1
+    ${PBS_O_HOME}/bin/GetNovelEntries.R ${ProjDir}/MaxQuant_txt "." ${UNIREFPROT} ${SIXFRAMEPROT} > ${LogDir}/GetNovelEntries.log 2>&1
 
 fi
 
@@ -69,7 +83,7 @@ fi
 # Check whether to create a blast database for proteome data
 if [ $MakeBlastDbProt == 1 ]; then
     
-    ${HOME}/bin/MakeBlastDb.sh ${InputType} "prot" ${TaxId} ${UNIREFPROT} ${SIXFRAMEPROT} > ${LogDir}/MakeBlastDb.log 2>&1
+    ${PBS_O_HOME}/bin/MakeBlastDb.sh ${InputType} "prot" ${TaxId} ${UNIREFPROT} ${SIXFRAMEPROT} > ${LogDir}/MakeBlastDb.log 2>&1
     
 fi
 
@@ -78,7 +92,7 @@ fi
 # Check whether to create a blast database for genome data
 if [ $MakeBlastDbNuc == 1 ]; then
     
-    ${HOME}/bin/MakeBlastDb.sh ${InputType} "nucl" ${TaxId} ${UNIREFGENE} ${SIXFRAMEGENE} >> ${LogDir}/MakeBlastDb.log 2>&1
+    ${PBS_O_HOME}/bin/MakeBlastDb.sh ${InputType} "nucl" ${TaxId} ${UNIREFGENE} ${SIXFRAMEGENE} >> ${LogDir}/MakeBlastDb.log 2>&1
     
 fi
 
@@ -91,11 +105,11 @@ fi
 # Check whether to blast the ORFs against taxon reference protein and RNA and against all uniprot and ncbi
 if [ $BlastDbBlasting == 1 ]; then
     
-    ${HOME}/bin/BlastDbBlasting.sh ${ProjDir}/Blast "prot" "all" "blastp" ${SIXFRAMEPROT} ${UNIREFPROT} ${Eval} ${NumAlign} ${MultiThreads} "ORFprot_vs_Refprot" > ${LogDir}/BlastDbBlasting.log 2>&1
-    ${HOME}/bin/BlastDbBlasting.sh ${ProjDir}/Blast "prot" ${ProjDir}/MaxQuant_txt/Novel_ORF.txt "blastp" ${SIXFRAMEPROT} ${ALLUNIPROT} ${Eval} ${NumAlign} ${MultiThreads} "ORFprot_vs_Uniprot" >> ${LogDir}/BlastDbBlasting.log 2>&1
-    ${HOME}/bin/BlastDbBlasting.sh ${ProjDir}/Blast "prot" ${ProjDir}/MaxQuant_txt/Novel_ORF.txt "blastp" ${SIXFRAMEPROT} ${ALLNCBIPROT} ${Eval} ${NumAlign} ${MultiThreads} "ORFprot_vs_NCBIprot" >> ${LogDir}/BlastDbBlasting.log 2>&1
-    ${HOME}/bin/BlastDbBlasting.sh ${ProjDir}/Blast "nucl" "all" "blastn" ${SIXFRAMEGENE} ${UNIREFGENE} ${Eval} ${NumAlign} ${MultiThreads} "ORFnucl_vs_Refrna" >> ${LogDir}/BlastDbBlasting.log 2>&1
-    ${HOME}/bin/BlastDbBlasting.sh ${ProjDir}/Blast "nucl" ${ProjDir}/MaxQuant_txt/Novel_ORF.txt "blastn" ${SIXFRAMEGENE} ${ALLNCBIRNA} ${Eval} ${NumAlign} ${MultiThreads} "ORFnucl_vs_NCBIrna" >> ${LogDir}/BlastDbBlasting.log 2>&1
+    ${PBS_O_HOME}/bin/BlastDbBlasting.sh ${ProjDir}/Blast "prot" "all" "blastp" ${SIXFRAMEPROT} ${UNIREFPROT} ${Eval} ${NumAlign} ${OMP_NUM_THREADS} "ORFprot_vs_Refprot" > ${LogDir}/BlastDbBlasting.log 2>&1
+    ${PBS_O_HOME}/bin/BlastDbBlasting.sh ${ProjDir}/Blast "prot" ${ProjDir}/MaxQuant_txt/Novel_ORF.txt "blastp" ${SIXFRAMEPROT} ${ALLUNIPROT} ${Eval} ${NumAlign} ${OMP_NUM_THREADS} "ORFprot_vs_Uniprot" >> ${LogDir}/BlastDbBlasting.log 2>&1
+    ${PBS_O_HOME}/bin/BlastDbBlasting.sh ${ProjDir}/Blast "prot" ${ProjDir}/MaxQuant_txt/Novel_ORF.txt "blastp" ${SIXFRAMEPROT} ${ALLNCBIPROT} ${Eval} ${NumAlign} ${OMP_NUM_THREADS} "ORFprot_vs_NCBIprot" >> ${LogDir}/BlastDbBlasting.log 2>&1
+    ${PBS_O_HOME}/bin/BlastDbBlasting.sh ${ProjDir}/Blast "nucl" "all" "blastn" ${SIXFRAMEGENE} ${UNIREFGENE} ${Eval} ${NumAlign} ${OMP_NUM_THREADS} "ORFnucl_vs_Refrna" >> ${LogDir}/BlastDbBlasting.log 2>&1
+    ${PBS_O_HOME}/bin/BlastDbBlasting.sh ${ProjDir}/Blast "nucl" ${ProjDir}/MaxQuant_txt/Novel_ORF.txt "blastn" ${SIXFRAMEGENE} ${ALLNCBIRNA} ${Eval} ${NumAlign} ${OMP_NUM_THREADS} "ORFnucl_vs_NCBIrna" >> ${LogDir}/BlastDbBlasting.log 2>&1
     
 fi
 
