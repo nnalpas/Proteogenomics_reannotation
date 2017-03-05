53ac8b9fc907dd5a7588ac5fd074c9ecf3aef4f4
nnalpas <nalpas.nicolas@gmail.com>
2017-03-05 22:47:51 +0100

    Script containing command for download and full installation of Blast package

---
+++ Linux_bash/Blast/Install_Blast.sh
@@ -0,0 +1,80 @@
+  553  cd Software/
+  554  wget 'ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/ncbi-blast-2.6.0+-src.zip'
+  555  unzip ncbi-blast-2.6.0+-src.zip 
+  556  unzip -h
+  557  zip -h
+  558  tar -h+
+  559  tar -h
+  560  tar -help
+  561  tar --help
+  562  tar -x ncbi-blast-2.6.0+-src.zip
+  563  ls
+  564  jar -h
+  565  jar 
+  566  gzip -h
+  567  yum -h
+  568  zum install zip unzip
+  569  yum install zip unzip
+  570  unzip
+  571  p7zip -h
+  572  zip
+  573  tar -xvf ncbi-blast-2.6.0+-src.zip
+  574  man tar
+  575  ls /
+  576  ls /etc/
+  577  ls /lib
+  578  module -t avail
+  579  module load devel/java_jdk/1.8.0u112
+  580  module list
+  581  jar -h
+  582  jar -xvf ncbi-blast-2.6.0+-src.zip 
+  583  ls
+  584  cd ncbi-blast-2.6.0+-src
+  585  ls
+  586  cd c++/
+  587  ls
+  588  ls src/
+  589  ls
+  590  more ./configure
+  591  ls
+  592  ./configure
+  593  chmod 770 ./configure
+  594  ./configure
+  595  chmod 770 ./configure.orig 
+  596  ./configure
+  597  ls src/
+  598  ls src/build-system/
+  599  chmod 770 src/build-system/configure
+  600  ./configure
+  601  chmod 770 ./scripts/common/impl/get_lock.sh
+  602  ./configure
+  603  ls
+  604  cd ReleaseMT/
+  605  ls
+  606  cd build/
+  607  ls
+  608  make all_r
+  609  chmod 770 /home-link/kxmna01/Software/ncbi-blast-2.6.0+-src/c++/scripts/common/impl/update_configurable.sh
+  610  make all_r
+  611  chmod 770 /home-link/kxmna01/Software/ncbi-blast-2.6.0+-src/c++/scripts/common/impl/run_with_lock.sh
+  612  make all_r
+  613  chmod 770 /home-link/kxmna01/Software/ncbi-blast-2.6.0+-src/c++/scripts/common/impl/make_lock_map.sh
+  614  make all_r
+  615  cd ..
+  616  ls
+  617  cd ..
+  618  ls
+  619  rm -rf ncbi-blast-2.6.0+-src
+  620  ls
+  621  history | tail -n 100
+  622  jar -xvf ncbi-blast-2.6.0+-src.zip 
+  623  chmod 770 -R ncbi-blast-2.6.0+-src
+  624  chmod --help
+  625  cd ncbi-blast-2.6.0+-src/c++/
+  626  ls
+  627  ./configure
+  628  cd ReleaseMT/build/
+  629  make all_r
+  630  ls
+  631  history | tail -n 80
+  632  history | tail -n 80 > ~/GitHub/Miscellaneous/Linux_bash/Blast/Install_Blast.sh
