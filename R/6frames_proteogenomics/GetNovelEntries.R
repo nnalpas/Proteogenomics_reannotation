5b41753aeea40d46828591150cc985e025dae2c4
nnalpas <nalpas.nicolas@gmail.com>
2017-04-10 14:45:33 +0200

    Update to new variable nomenclature

--- R/6frames_proteogenomics/GetNovelEntries.R
+++ R/6frames_proteogenomics/GetNovelEntries.R
@@ -11,8 +11,8 @@
 rm(list = ls())
 
 # Define current time
-date.str <- format(Sys.time(), "%Y-%m-%d")
-print(paste("Start ", date.str, sep = ""))
+date_str <- format(Sys.time(), "%Y-%m-%d")
+print(paste("Start ", date_str, sep = ""))
 
 
 
@@ -27,16 +27,16 @@ if (length(args) != 4) {
 }
 
 # Define the work space
-work.space <- args[1]
-setwd(work.space)
-print(paste("Working directory: ", work.space, sep = ""))
+work_space <- args[1]
+setwd(work_space)
+print(paste("Working directory: ", work_space, sep = ""))
 
 # Select the maxquant txt folder
-txt.dir <- args[2]
+txt_dir <- args[2]
 
 # List the fasta files that need to be imported
-fasta.file <- c(args[3], args[4])
-names(fasta.file) <- c("Known", "Novel")
+fasta_file <- c(args[3], args[4])
+names(fasta_file) <- c("Known", "Novel")
 
 # Define the current user
 user <- Sys.info()[["user"]]
@@ -62,14 +62,14 @@ source(
 # Load the required packages (or install if not already in library)
 # Note: the select function from dplyr and UniProt.ws is overlapping,
 # therefore order of package loading is important
-loadpackage(plyr)
-loadpackage(dplyr)
-loadpackage(seqinr)
-loadpackage(bit64)
-loadpackage(magrittr)
-loadpackage(data.table)
-loadpackage(splitstackshape)
-loadpackage(stringr)
+load_package(plyr)
+load_package(dplyr)
+load_package(seqinr)
+load_package(bit64)
+load_package(magrittr)
+load_package(data.table)
+load_package(splitstackshape)
+load_package(stringr)
 
 
 
@@ -77,25 +77,25 @@ loadpackage(stringr)
 
 # Import the maxquant evidence table
 evid <- maxquant.read(
-    path = txt.dir,
+    path = txt_dir,
     name = "evidence.txt",
     integer64 = "double")
 
 # Import all fasta file data and store into list
-fasta.list <- list()
-for (x in 1:length(fasta.file)) {
+fasta_list <- list()
+for (x in 1:length(fasta_file)) {
     
     # Import the current fasta file
     tmp <- read.fasta(
-        file = fasta.file[x], seqtype = "AA", as.string = TRUE)
+        file = fasta_file[x], seqtype = "AA", as.string = TRUE)
     
     # Include imported fasta into the list
-    fasta.list[names(fasta.file)[x]] <- list(tmp)
+    fasta_list[names(fasta_file)[x]] <- list(tmp)
     
 }
 
 # Clean-up
-rm(fasta.file)
+rm(fasta_file)
 
 
 
@@ -109,69 +109,69 @@ data <- evid %>%
     base::as.data.frame(., stringsAsFactors = FALSE)
 
 # Format the fasta files
-names(fasta.list$Known) %<>%
+names(fasta_list$Known) %<>%
     sub(pattern = ".+\\|(.+)\\|.+", replacement = "\\1", x = .)
 
 # Locate all the peptide within associated proteins
-pep.loc <- list()
-pep.loc[["Known"]] <- pept.locate(
+pep_loc <- list()
+pep_loc[["Known"]] <- pept.locate(
     data = data, peptide = "Sequence",
-    proteins = "Proteins", fasta = fasta.list$Known) %>%
+    proteins = "Proteins", fasta = fasta_list$Known) %>%
     dplyr::filter(., !is.na(start))
-pep.loc[["Novel"]] <- pept.locate(
+pep_loc[["Novel"]] <- pept.locate(
     data = data, peptide = "Sequence",
-    proteins = "Proteins", fasta = fasta.list$Novel) %>%
+    proteins = "Proteins", fasta = fasta_list$Novel) %>%
     dplyr::filter(., !is.na(start))
-pep.loc[["Contaminant"]] <- data %>%
+pep_loc[["Contaminant"]] <- data %>%
     dplyr::filter(., grepl("^CON__", Proteins)) %>%
     set_colnames(c("pep", "prot")) %>%
     dplyr::mutate(., start = NA_integer_, end = NA_integer_)
-pep.loc[["Reverse"]] <- evid %>%
+pep_loc[["Reverse"]] <- evid %>%
     dplyr::filter(., Reverse == "+") %>%
     dplyr::select(., Sequence, Proteins) %>%
     set_colnames(c("pep", "prot")) %>%
     dplyr::mutate(., start = NA_integer_, end = NA_integer_)
 
 # New dataframe to hold info about fasta of origin for each sequence
-evid.match <- evid %>%
+evid_match <- evid %>%
     dplyr::mutate(
         .,
         group = ifelse(
-            test = Sequence %in% pep.loc$Known$pep,
+            test = Sequence %in% pep_loc$Known$pep,
             yes = "Known",
             no = ifelse(
-                test = Sequence %in% pep.loc$Contaminant$pep,
+                test = Sequence %in% pep_loc$Contaminant$pep,
                 yes = "Contaminant",
                 no = ifelse(
-                    test = Sequence %in% pep.loc$Novel$pep,
+                    test = Sequence %in% pep_loc$Novel$pep,
                     yes = "Novel",
                     no = ifelse(
-                        test = Sequence %in% pep.loc$Reverse$pep,
+                        test = Sequence %in% pep_loc$Reverse$pep,
                         yes = "Reverse",
                         no = NA_character_))))) %>%
     base::as.data.frame(., stringAsFactors = TRUE)
 
 # Print warning for unidentified sequence origin
 print(paste(
-    "There are", length(which(is.na(evid.match$group))),
+    "There are", length(which(is.na(evid_match$group))),
     "NA values, these need to be checked!", sep = " "))
 
 # Print the repartition of peptide per group
-print(table(evid.match$group, useNA = "always"))
+print(table(evid_match$group, useNA = "always"))
 
 # Save the group mapping data
 saveRDS(
-    object = evid.match,
+    object = evid_match,
     file = "Sequence_group_mapping.RDS")
 
 # Save the peptide location data
 saveRDS(
-    object = pep.loc,
+    object = pep_loc,
     file = "Peptides_location.RDS")
 
 # Export complete evidence info for these novel evidence
 write.table(
-    x = evid.match[evid.match$group == "Novel", ],
+    x = evid_match[evid_match$group == "Novel", ],
     file = "Novel_evidence.txt",
     quote = FALSE,
     sep = "\t",
@@ -180,7 +180,7 @@ write.table(
 
 # Export complete evidence info for these novel evidence
 write.table(
-    x = evid.match %>%
+    x = evid_match %>%
         dplyr::filter(., group == "Novel") %>%
         cSplit(
             indt = ., splitCols = "Proteins", sep = ";",
