a0ff52786eb57e2c50fe571574181bc42e362d2a
nnalpas <nalpas.nicolas@gmail.com>
2017-04-11 16:55:59 +0200

    Compute the reference protein entries coordinates based on blast versus ORF of known coordinates

--- R/6frames_proteogenomics/Genomic_position_from_blast.R
+++ R/6frames_proteogenomics/Genomic_position_from_blast.R
@@ -99,38 +99,97 @@ fasta <- opt$fasta %>%
     seqinr::read.fasta(
         file = ., seqtype = "AA", as.string = TRUE) %>%
     set_names(uni_id_clean(names(.)))
-    
+
 # Get all protein IDs
 uni_ids <- names(fasta) %>%
     as.character(.) %>%
     unique(.)
 
 # Import the Blast results
-blast_data <- read.table(
-    file = opt$blast,
-    header = FALSE,
-    sep = "\t",
-    quote = "",
-    col.names = c(
-        "qseqid", "sseqid", "pident", "nident", "mismatch", "length",
-        "gapopen", "qstart", "qend", "sstart", "send", "evalue",
-        "bitscore", "score"),
-    as.is = TRUE) %>%
+blast_data <- opt$blast%>%
+    as.character(.) %>%
+    read.table(
+        file = .,
+        header = FALSE,
+        sep = "\t",
+        quote = "",
+        col.names = c(
+            "qseqid", "sseqid", "pident", "nident", "mismatch", "length",
+            "gapopen", "qstart", "qend", "sstart", "send", "evalue",
+            "bitscore", "score"),
+        as.is = TRUE) %>%
     dplyr::mutate(
         .,
         qseqid = uni_id_clean(qseqid),
         sseqid = uni_id_clean(sseqid))
 
 # Import the genomic position data
-orf_pos <- read.table(
-    file = opt$genomic_position, header = TRUE, sep = "\t", quote = "")
+orf_pos <- opt$genomic_position %>%
+    as.character(.) %>%
+    read.table(
+        file = ., header = TRUE, sep = "\t", quote = "")
 
 
 
 ### Get target ID position based on blast --------------------------------
 
 # Determine the best blast for each queried protein
-best_blast_data <- best_blast(data = blast_data, key = "qseqid")
+best_blast_data <- best_blast(data = blast_data, key = "qseqid") %>%
+    dplyr::filter(., evalue < 0.0001)
 
+# Give warning when a single best blast could not be found per query
+if (any(best_blast_data$best_count != 1)) {
+    
+    warning(paste(
+        "There are",
+        length(unique(
+            best_blast_data[best_blast_data$best_count != 1, "qseqid"])),
+        "queries without a single best blast!", sep = " "))
+    
+}
+
+# Add the orf position to the blast data
+best_blast_pos <- best_blast_data %>%
+    dplyr::left_join(x = ., y = orf_pos, by = c("sseqid" = "id"))
+
+# Compute aa and nucleotide length of reference entries
+ref_pos <- lapply(X = fasta, FUN = nchar) %>%
+    plyr::ldply(., "data.frame") %>%
+    set_colnames(c("id", "aa_length")) %>%
+    dplyr::mutate(., nucl_length = aa_length * 3)
+
+# Get the genomic start position for reference entries
+best_blast_pos %<>%
+    dplyr::mutate(., ref_start = case_when(
+        .$strand == 1 ~ (.$start + (.$sstart * 3) - (.$qstart * 3)),
+        .$strand == -1 ~ (.$start - (.$sstart * 3) + (.$qstart * 3))))
+
+# Add the reference entries start position
+ref_pos %<>%
+    dplyr::left_join(
+        x = .,
+        y = best_blast_pos %>%
+            dplyr::select(., qseqid, ref_start, strand, frame, best_count),
+        by = c("id" = "qseqid"))
+
+# Compute the reference entries end position
+ref_pos %<>%
+    dplyr::mutate(., ref_end = case_when(
+        is.na(.$strand) ~ NA_integer_,
+        .$strand == 1 ~ as.integer(.$ref_start + (.$aa_length * 3) - 1),
+        .$strand == -1 ~ as.integer(.$ref_start - (.$aa_length * 3) + 1))) %>%
+    dplyr::select(
+        ., id, ref_start, ref_end, nucl_length, aa_length, strand, frame)
+colnames(ref_pos) %<>%
+    sub("^ref_", "", .)
+
+# Export the reference entries coordinates data
+write.table(
+    x = ref_pos, file = opt$output,
+    quote = FALSE, sep = "\t",
+    row.names = FALSE, col.names = TRUE)
+
+# Define end time
+print(paste("Complete", format(Sys.time(), "%Y-%m-%d %H:%M:%S")))
 
 
