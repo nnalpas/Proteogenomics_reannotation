3bfe52d357e7e40b4563ba8a523679045e1c2cfc
nnalpas <nalpas.nicolas@gmail.com>
2017-11-27 12:35:39 +0100

    Adapt script so that it can be run interactively and optimise how to determine the best reciprocal best hits (same subject id and sequence)

--- R/6frames_proteogenomics/Reciprocal_best_blast.R
+++ R/6frames_proteogenomics/Reciprocal_best_blast.R
@@ -22,50 +22,70 @@ user <- Sys.info()[["user"]]
 ### List of required packages -----------------------------------------------
 
 # Source the custom user functions
-#source(
-#    file = paste(
-#        "C:/Users",
-#        user,
-#        "Documents/GitHub/Miscellaneous/R/General/General_function.R",
-#        sep = "/"))
-source(
-    file = paste(
-        "/home-link",
-        user,
-        "bin/General_function.R",
-        sep = "/"))
+if (interactive()) {
+    source(
+        file = paste(
+            "C:/Users",
+            user,
+            "Documents/GitHub/Miscellaneous/R/6frames_proteogenomics/helper.R",
+            sep = "/"))
+} else {
+    source(
+        file = paste(
+            "/home-link",
+            user,
+            "bin/helper.R",
+            sep = "/"))
+}
 
 # Load the required packages (or install if not already in library)
-load_package(plyr)
-load_package(dplyr)
-load_package(magrittr)
-load_package(data.table)
-load_package(splitstackshape)
-load_package(stringr)
-load_package(optparse)
+load_package("plyr")
+load_package("dplyr")
+load_package("magrittr")
+load_package("data.table")
+load_package("splitstackshape")
+load_package("stringr")
+load_package("optparse")
 
 
 
 ### Parameters setting up ------------------------------------------------
 
-# Define the list of command line parameters
-option_list <- list(
-    make_option(
-        opt_str = c("-b", "--blast"),
-        type = "character", default = NULL, 
-        help = "Blast data file name", metavar = "character"),
-    make_option(
-        opt_str = c("-r", "--reciprocal_blast"),
-        type = "character", default = NULL, 
-        help = "Reciprocal blast data file name", metavar = "character"),
-    make_option(
-        opt_str = c("-o", "--output"),
-        type = "character", default = NULL, 
-        help = "Output directory", metavar = "character"))
-
-# Parse the parameters provided on command line by user
-opt_parser <- OptionParser(option_list = option_list)
-opt <- parse_args(opt_parser)
+# Define input parameters (interactively or from command line)
+if (interactive()) {
+    
+    # Define the list of input parameters
+    opt <- list(
+        blast = choose.files(
+            caption = "Choose input Blast results",
+            multi = FALSE),
+        reciprocal_blast = choose.files(
+            caption = "Choose input reciprocal Blast results",
+            multi = FALSE),
+        output = ".")
+    
+} else {
+    
+    # Define the list of command line parameters
+    option_list <- list(
+        make_option(
+            opt_str = c("-b", "--blast"),
+            type = "character", default = NULL, 
+            help = "Blast data file name", metavar = "character"),
+        make_option(
+            opt_str = c("-r", "--reciprocal_blast"),
+            type = "character", default = NULL, 
+            help = "Reciprocal blast data file name", metavar = "character"),
+        make_option(
+            opt_str = c("-o", "--output"),
+            type = "character", default = NULL, 
+            help = "Output directory", metavar = "character"))
+    
+    # Parse the parameters provided on command line by user
+    opt_parser <- OptionParser(option_list = option_list)
+    opt <- parse_args(opt_parser)
+    
+}
 
 # Check whether inputs parameter was provided
 if (is.null(opt$blast) | is.null(opt$reciprocal_blast)){
@@ -87,7 +107,7 @@ if (is.null(opt$output)){
 
 
 
-### Data import and best blast computation -------------------------------
+### Data import ----------------------------------------------------------
 
 # Import the Blast results
 blast_data <- blast_read(file = opt$blast, blast_format = "6") %>%
@@ -95,9 +115,6 @@ blast_data <- blast_read(file = opt$blast, blast_format = "6") %>%
         .,
         qseqid = uni_id_clean(qseqid),
         sseqid = uni_id_clean(sseqid))
-
-# Get the best blast match for each query
-best_blast_data <- best_blast(data = blast_data, key = "qseqid")
 
 # Import the reciprocal Blast results
 reciproc_data <- blast_read(
@@ -107,35 +124,52 @@ reciproc_data <- blast_read(
         qseqid = uni_id_clean(qseqid),
         sseqid = uni_id_clean(sseqid))
 
-# Get the best reciprocal blast match for each query
-best_reciproc_data <- best_blast(data = reciproc_data, key = "qseqid")
-
 
 
 ### Reciprocal best blast hits identification ----------------------------
+
+# Get the best blast match for each query
+best_blast_data <- best_blast(data = blast_data, key = "qseqid")
+
+# Get the best reciprocal blast match for each query
+best_reciproc_data <- best_blast(data = reciproc_data, key = "qseqid")
 
 # Merge the best blast and best reciprocal data
 blast_merge <- best_blast_data %>%
     dplyr::left_join(
         x = .,
         y = best_reciproc_data,
-        by = c("qseqid" = "sseqid"),
+        by = c("qseqid" = "sseqid", "sseqid" = "qseqid"),
         suffix = c("_blast", "_reciproc"))
 
 # Determine which entry have a reciprocal best hits
+# this requires checking how many distinct subject ID are matched per query id
+# as well as checking how many distinct subject sequence exist
+# (the same sequence can have different IDs across different taxon)
 blast_merge_confirmed <- blast_merge %>%
-    dplyr::filter(
+    dplyr::group_by(., qseqid) %>%
+    dplyr::mutate(
         .,
-        is.na(qseqid_reciproc) |
-            sseqid == qseqid_reciproc) %>%
-    dplyr::mutate(., best_reciprocal_type = case_when(
-        is.na(.$qseqid_reciproc) ~ "None",
-            .$best_count_reciproc == 1 ~ "Single",
-        TRUE ~ "Multiple"))
+        final_count = n_distinct(sseqid),
+        seq_count = n_distinct(qseq_reciproc)) %>%
+    dplyr::ungroup(.) %>%
+    dplyr::mutate(
+        ., 
+        best_reciprocal_type = dplyr::case_when(
+            is.na(sseqid) ~ "None",
+            final_count == 1 ~ "Single",
+            TRUE ~ "Multiple"),
+        comment = dplyr::case_when(
+            seq_count == 1 ~ "Single",
+            seq_count > 1 ~ "Multiple"))
+
+
+
+### Results export -------------------------------------------------------
 
 # Export the best hits results
 write.table(
-    x = best_blast_data,
+    x = blast_merge_confirmed,
     file = paste0(
         opt$output, "/Best_Reciproc_Blast_",
         basename(opt$reciprocal_blast)),
@@ -146,10 +180,10 @@ write.table(
 
 # Export the blast ID map results
 write.table(
-    x = best_blast_data %>%
-        dplyr::select(., qseqid, sseqid),
+    x = blast_merge_confirmed %>%
+        dplyr::select(., qseqid, sseqid, best_reciprocal_type, comment),
     file = paste0(
-        opt$output, "/Reciproc_Blast_cross-map_",
+        opt$output, "/Best_Reciproc_Blast_cross-map_",
         basename(opt$reciprocal_blast)),
     quote = FALSE,
     sep = "\t",
