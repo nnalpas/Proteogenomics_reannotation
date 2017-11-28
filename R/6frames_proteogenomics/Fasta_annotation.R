67c98f407ea1daebf1df150cfa3c61b744d0d22f
nnalpas <nalpas.nicolas@gmail.com>
2017-11-28 18:06:30 +0100

    New script to retrieve user specified annotation for entries in fasta file via uniprot.ws package

---
+++ R/6frames_proteogenomics/Fasta_annotation.R
@@ -0,0 +1,182 @@
+#!/usr/bin/env Rscript
+
+# This script generates a GRange for the genome of interest
+
+
+
+### Environment set-up ---------------------------------------------------
+
+# Start with clean environment
+rm(list = ls())
+
+# Define current time
+date_str <- format(Sys.time(), "%Y-%m-%d")
+print(paste("Start", format(Sys.time(), "%Y-%m-%d %H:%M:%S")))
+
+# Define the current user
+user <- Sys.info()[["user"]]
+
+
+
+### List of required packages -----------------------------------------------
+
+# Source the custom user functions
+if (interactive()) {
+    source(
+        file = paste(
+            "C:/Users",
+            user,
+            "Documents/GitHub/Miscellaneous/R/6frames_proteogenomics/helper.R",
+            sep = "/"))
+} else {
+    source(
+        file = paste(
+            "/home-link",
+            user,
+            "bin/helper.R",
+            sep = "/"))
+}
+
+# Load the required packages (or install if not already in library)
+load_package("plyr")
+load_package("dplyr")
+load_package("magrittr")
+load_package("data.table")
+load_package("splitstackshape")
+load_package("stringr")
+load_package("optparse")
+load_package("seqinr")
+load_package("UniProt.ws")
+
+
+
+### Parameters setting up ------------------------------------------------
+
+# Define input parameters (interactively or from command line)
+if (interactive()) {
+    
+    # Define the list of input parameters
+    opt <- list(
+        fasta = choose.files(
+            caption = "Choose Fasta file of the known protein (UniProt)!",
+            multi = FALSE),
+        taxon = readline(
+            prompt = "Provide the taxon identifier!"),
+        columns = readline(
+            prompt = "Provide the annotation columns to retrieve!"),
+        key = readline(
+            prompt = "Provide the key name to use for cross-annotation!"),
+        output = readline(
+            prompt = "Define the output directory!"))
+    
+} else {
+    
+    # Define the list of command line parameters
+    option_list <- list(
+        make_option(
+            opt_str = c("-f", "--fasta"),
+            type = "character", default = NULL,
+            help = "Known protein fasta file",
+            metavar = "character"),
+        make_option(
+            opt_str = c("-t", "--taxon"),
+            type = "integer", default = NULL,
+            help = "The taxon identifier for the species of interest",
+            metavar = "character"),
+        make_option(
+            opt_str = c("-c", "--columns"),
+            type = "character", default = NULL,
+            help = "The annotation column to retrieve!",
+            metavar = "character"),
+        make_option(
+            opt_str = c("-k", "--key"),
+            type = "character", default = NULL,
+            help = "The key name to use for cross-annotation!",
+            metavar = "character"),
+        make_option(
+            opt_str = c("-o", "--output"),
+            type = "character", default = "", 
+            help = "Output file name [default= %default]",
+            metavar = "character"))
+    
+    # Parse the parameters provided on command line by user
+    opt_parser <- OptionParser(option_list = option_list)
+    opt <- parse_args(opt_parser)
+    
+}
+
+# Check whether inputs parameter were provided
+if (is.null(opt$fasta) | is.null(taxon) | is.null(columns) | is.null(key)) {
+    
+    print_help(opt_parser)
+    
+}
+
+# Check whether output parameter was provided
+if (opt$output == "") {
+    
+    opt$output <- dirname(opt$fasta)
+    warning(paste0(
+        "Output results to '",
+        opt$output,
+        "'!"))
+    
+}
+
+# Create output directory if not already existing
+dir.create(dirname(opt$output))
+
+
+
+### Data import ----------------------------------------------------------
+
+# Import the fasta files
+fasta <- seqinr::read.fasta(
+    file = opt$fasta, seqtype = "AA", as.string = TRUE)
+
+
+
+### Obtain protein annotation from UniProt -------------------------------
+
+# Clean up the UniProtKB
+names(fasta) <- uni_id_clean(names(fasta))
+
+# Create UniProt.ws object for submitted taxon ID
+up <- UniProt.ws(taxId = as.numeric(as.character(taxon)))
+
+# Get all the columns specified by user
+data <- UniProt.ws::select(
+    x = up, keys = names(fasta), columns = columns, keytype = keytype)
+
+# Extract the gene name and locus from the GENES column if existing
+if (any(colnames(data) %in% c("GENES"))) {
+    data_final <- data %>%
+        dplyr::mutate(
+            .,
+            `GENE-NAME` = sub(
+                "^(.*?)( .*)? BSU\\d+.*$", "\\1", GENES, perl = TRUE),
+            LOCUS = sub(
+                "^.* (BSU\\d+).*$", "\\1", GENES, perl = TRUE),
+            `ALT-GENE-NAME` = sub(
+                "^.*?( (.*))? BSU\\d+.*$", "\\1", GENES, perl = TRUE))
+} else {
+    data_final <- data
+}
+
+
+
+### Results export -------------------------------------------------------
+
+# Export the bsu ideogram for reuse at later stage
+write.table(
+    x = data_final,
+    file = paste0(
+        opt$output, "/",
+        sub("\\.(fasta|fa)", "", basename(opt$fasta)),
+        "_annotation.txt"),
+    quote = FALSE, sep = "\t", row.names = FALSE, col.names = TRUE)
+
+# Define end time
+print(paste("Complete", format(Sys.time(), "%Y-%m-%d %H:%M:%S")))
+
+
