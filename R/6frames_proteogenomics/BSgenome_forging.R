ca0625d2b93af33c46d20927cb95e210730e2ad7
nnalpas <nalpas.nicolas@gmail.com>
2017-12-05 15:22:30 +0100

    New script to forge a BSgenome package (requires seed file and directory path to fasta sequences as well as output directory)

---
+++ R/6frames_proteogenomics/BSgenome_forging.R
@@ -0,0 +1,126 @@
+#!/usr/bin/env Rscript
+
+# This script generates a BSgenome package
+
+
+
+### Environment set-up ---------------------------------------------------
+
+# Start with clean environment
+rm(list = ls())
+
+# Define current time
+date_str <- format(Sys.time(), "%Y-%m-%d")
+print(paste("Start", format(Sys.time(), "%Y-%m-%d %H:%M:%S")))
+
+# Define the current user
+user <- Sys.info()[["user"]]
+
+
+
+### List of required packages -----------------------------------------------
+
+# Source the custom user functions
+if (interactive()) {
+    source(
+        file = paste(
+            "C:/Users",
+            user,
+            "Documents/GitHub/Proteogenomics_reannotation/",
+            "R/6frames_proteogenomics/helper.R",
+            sep = "/"))
+} else {
+    source(
+        file = paste(
+            "/home-link",
+            user,
+            "bin/helper.R",
+            sep = "/"))
+}
+
+# Load the required packages (or install if not already in library)
+load_package("plyr")
+load_package("dplyr")
+load_package("magrittr")
+load_package("data.table")
+load_package("splitstackshape")
+load_package("stringr")
+load_package("optparse")
+load_package("seqinr")
+load_package("BSgenome")
+
+
+
+### Parameters setting up ------------------------------------------------
+
+# Define input parameters (interactively or from command line)
+if (interactive()) {
+    
+    # Define the list of input parameters
+    opt <- list(
+        seed = choose.files(
+            caption = "Choose the seed file!",
+            multi = FALSE),
+        fasta = choose.dir(
+            caption = "Choose the directory containing the fasta files!"),
+        output = choose.dir(
+            caption = "Choose the output directory!"))
+    
+} else {
+    
+    # Define the list of command line parameters
+    option_list <- list(
+        make_option(
+            opt_str = c("-s", "--seed"),
+            type = "character", default = NULL,
+            help = "BSgenome seed file",
+            metavar = "character"),
+        make_option(
+            opt_str = c("-f", "--fasta"),
+            type = "character", default = NULL,
+            help = "Fasta sequences directory",
+            metavar = "character"),
+        make_option(
+            opt_str = c("-o", "--output"),
+            type = "character", default = NULL,
+            help = "Output directory",
+            metavar = "character"))
+    
+    # Parse the parameters provided on command line by user
+    opt_parser <- OptionParser(option_list = option_list)
+    opt <- parse_args(opt_parser)
+    
+}
+
+# Check whether inputs parameter were provided
+if (is.null(opt$seed) | opt$seed == "") {
+    
+    print_help(opt_parser)
+    stop("BSgenome seed file required!")
+    
+}
+if (is.null(opt$fasta) | opt$fasta == "") {
+    
+    print_help(opt_parser)
+    stop("Fasta sequence directory required!")
+    
+}
+if (is.null(opt$output) | opt$output == "") {
+    
+    print_help(opt_parser)
+    stop("Output directory required!")
+    
+}
+
+
+
+### Forge target package -------------------------------------------------
+
+# Make a BSgenome data package using the seed file
+forgeBSgenomeDataPkg(
+    x = opt$seed, seqs_srcdir = opt$fasta, destdir = opt$output)
+
+# Define end time
+print(paste("Complete", format(Sys.time(), "%Y-%m-%d %H:%M:%S")))
+
+
