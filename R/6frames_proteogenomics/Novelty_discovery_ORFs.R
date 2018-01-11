31302ef7130a43383fb21e480aeef338970af678
nnalpas <nalpas.nicolas@gmail.com>
2018-01-11 11:31:06 +0100

    Include neighbour analysis for novel ORF (and visualisation), also allow multithreading

--- R/6frames_proteogenomics/Novelty_discovery_ORFs.R
+++ R/6frames_proteogenomics/Novelty_discovery_ORFs.R
@@ -73,6 +73,7 @@ if (interactive()) {
         orf_grange = choose.files(
             caption = "Choose ORF entries grange (.RDS) file!",
             multi = FALSE),
+        threads = readline(prompt = "How many cores to use?") %>% as.integer(),
         output = readline(
             prompt = "Define the output directory!"))
     
@@ -94,6 +95,11 @@ if (interactive()) {
             opt_str = c("-n", "--orf_grange"),
             type = "character", default = NULL,
             help = "ORF entries grange file",
+            metavar = "character"),
+        make_option(
+            opt_str = c("-t", "--threads"),
+            type = "integer", default = "",
+            help = "Number of cores to use",
             metavar = "character"),
         make_option(
             opt_str = c("-o", "--output"),
@@ -123,6 +129,17 @@ if (is.null(opt$orf_grange)) {
     
     print_help(opt_parser)
     stop("The input ORF entries grange must be supplied!")
+    
+}
+if (is.null(opt$threads)) {
+    
+    print_help(opt_parser)
+    stop("Number of threads is null!")
+    
+} else {
+    
+    registerDoParallel(cores = opt$threads)
+    print(paste("Number of threads registered:", getDoParWorkers()))
     
 }
 
@@ -158,7 +175,93 @@ orf_grange <- opt$orf_grange %>%
 
 
 
+### ORF neighbour analysis -----------------------------------------------
+
+# Identify the putative novel ORFs
+targets <- evid_reason %>%
+    dplyr::filter(., Database == "Novel") %>%
+    .[["Proteins"]] %>%
+    strsplit(x = ., split = ";", fixed = TRUE) %>%
+    unlist(.) %>%
+    unique(.)
+
+# Remove target IDs not present in genomic range
+targets_filt <- targets[targets %in% names(orf_grange)]
+
+# Define the subset of novel ORF that were found expressed
+sub_orf_grange <- orf_grange[targets_filt]
+
+# Identify all neighbours entries to the novel ORFs
+neighbours_list <- gr_near_dist(
+    query = sub_orf_grange,
+    subject = ref_grange,
+    near_type = c("nearest","precede", "follow"),
+    select = "all")
+
+
+
+
+### Data visualisation and export ----------------------------------------
+
+# Loop through each neighbour type
+for (x in names(neighbours_list)) {
+    
+    # Generate frequency of neighbouring entries distance (between ORF and ref)
+    toplot <- neighbours_list[[x]] %>%
+        #dplyr::mutate(
+        #    .,
+        #    dist = factor(
+        #        x = dist,
+        #        levels = seq(0, max(dist), by = 1),
+        #        labels = seq(0, max(dist), by = 1),
+        #        ordered = TRUE)) %>%
+        plyr::ddply(
+            .data = .,
+            .variables = .(queryStrand, dist),
+            .fun = function(piece) {
+                summarise(piece, freq = length(queryID)) },
+            .drop = FALSE,
+            .parallel = TRUE)
+    
+    # Visualise frequency as histogram
+    pl <- plots_hist(
+        data = toplot,
+        key = "dist",
+        value = "freq",
+        group = "dist",
+        fill = "queryStrand",
+        posit = "stack",
+        main = paste("Frequency of distance between", x, "entries"),
+        xlabel = "Distance (bp)",
+        ylabel = "Count",
+        legend = "bottom",
+        bw = TRUE,
+        xdir = "vertical",
+        auto_scale = 10)
+    print(pl[[1]])
+    pl <- plots_hist(
+        data = toplot %>% dplyr::filter(., as.numeric(dist) < 101),
+        key = "dist",
+        value = "freq",
+        group = "dist",
+        fill = "queryStrand",
+        posit = "stack",
+        main = paste("Frequency of  0 - 100 bp between", x, "entries"),
+        xlabel = "Distance (bp)",
+        ylabel = "Count",
+        legend = "bottom",
+        bw = TRUE,
+        xdir = "vertical")
+    print(pl[[1]])
+    
+}
+
+
+
 ### END ------------------------------------------------------------------
+
+# Close the cluster
+stopImplicitCluster()
 
 # Time scripts end
 print(paste("Completed ", format(Sys.time(), "%Y-%m-%d"), sep = ""))
