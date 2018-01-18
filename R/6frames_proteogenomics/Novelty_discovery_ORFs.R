f97d13b322eb0047681bf9cc6748809fea755031
nnalpas <nalpas.nicolas@gmail.com>
2018-01-18 22:30:30 +0100

    Include the RBS motif sequence analysis into script (based on previous code)

--- R/6frames_proteogenomics/Novelty_discovery_ORFs.R
+++ R/6frames_proteogenomics/Novelty_discovery_ORFs.R
@@ -56,6 +56,9 @@ load_package("gridExtra")
 load_package("purrr")
 load_package("foreach")
 load_package("doParallel")
+load_package("motifRG")
+load_package("ggseqlogo")
+load_package("BSgenome.Bsubtilis.EMBL.AL0091263")
 
 
 
@@ -288,6 +291,277 @@ if (exists("operon_grange")) {
             subjectStrand = as.character(strand(operon_grange)[subjectHits]))
     
 }
+
+
+
+### RBS motif analysis ---------------------------------------------------
+
+# Get the Bsu genome object
+bsu <- BSgenome.Bsubtilis.EMBL.AL0091263
+
+# Create new GRange object to study nucleotide frequence of start codon
+# on positive strand
+plus_grange <- subset(ref_grange, strand == "+")
+names(plus_grange) %<>%
+    sub("^", "start_", .)
+end(plus_grange) <- (start(plus_grange) + 2)
+
+# Create new GRange object to study nucleotide frequence of start codon
+# on negative strand
+minus_grange <- subset(ref_grange, strand == "-")
+names(minus_grange) %<>%
+    sub("^", "start_", .)
+start(minus_grange) <- (end(minus_grange) - 2)
+
+# Compile positive and negative strand entries
+start_codon <- c(plus_grange, minus_grange)
+
+# Get the nucleotide sequence associated with the start codon
+start_codon_seq <- getSeq(x = bsu, names = start_codon)
+
+# Check frequency of start codon
+start_codon_freq <- trinucleotideFrequency(
+    x = start_codon_seq, step = 1, as.prob = FALSE, as.array = FALSE,
+    fast.moving.side = "right", with.labels = TRUE,
+    simplify.as = "collapsed")
+
+# Reformat and calculate percentage of count per start codon
+start_codon_freq %<>%
+    as.data.frame(.) %>%
+    set_colnames("Count") %>%
+    dplyr::mutate(
+        .,
+        Codon = rownames(.),
+        Percentage = Count / sum(Count))
+
+# Keep only the start codon with frequency higher than 1%
+used_start <- start_codon_freq %>%
+    dplyr::filter(., Percentage > 0.01) %>%
+    .[["Codon"]]
+
+# Compute the MS/MS count for all known protein and ORF within range of
+# the target and for each raw file
+msms_count <- evid_reason %>%
+    dplyr::filter(., group == "Known") %>%
+    dplyr::group_by(., `Leading Proteins`, `Raw file`) %>%
+    dplyr::summarise(., Count = sum(`MS/MS Count`)) %>%
+    set_colnames(c("Proteins", "Raw", "Count")) %>%
+    dplyr::ungroup(.) %>%
+    dplyr::group_by(., Proteins) %>%
+    dplyr::summarise(., Count = median(Count)) %>%
+    dplyr::arrange(., Count) %>%
+    cSplit(
+        indt = ., splitCols = "Proteins", sep = ";",
+        direction = "long", fixed = TRUE) %>%
+    dplyr::filter(., !grepl("^seq_", Proteins))
+
+# Get the highest and lowest expressed protein
+low_express <- msms_count %>%
+    dplyr::filter(., Count <= median(Count)) %>%
+    .[["Proteins"]] %>%
+    as.character(.)
+high_express <- msms_count %>%
+    dplyr::filter(., Count > median(Count)) %>%
+    .[["Proteins"]] %>%
+    as.character(.)
+
+# Create list of protein stratified by abundance
+ref_expr_list <- list(
+    `Low abundant` = low_express, `High abundant` = high_express)
+
+# Loop through the stratified list of proteins
+rbs_motifs <- list()
+rbs_motifs_score <- list()
+for (i in 1:length(ref_expr_list)) {
+    
+    # Create foreground GRange object (+/- X bp of start)
+    # to study RBS nucleotide frequence for entries on + and - strand
+    pos_grange <- subset(
+        ref_grange, strand == "+" & id %in% ref_expr_list[[i]])
+    ranges(pos_grange) <- IRanges(
+        start = (start(pos_grange) - 30),
+        end = (start(pos_grange) + 2))
+    neg_grange <- subset(
+        ref_grange, strand == "-" & id %in% ref_expr_list[[i]])
+    ranges(neg_grange) <- IRanges(
+        start = (end(neg_grange) - 2),
+        end = (end(neg_grange) + 30))
+    
+    # Concatenate the entries from positive and negative strand
+    fg_grange <- c(pos_grange, neg_grange)
+    names(fg_grange) <- sub("^", "fg_", fg_grange$id)
+    
+    # Get the nucleotide sequence associated with the grange object
+    fg_seq <- getSeq(x = bsu, names = fg_grange)
+    fg_seq_list <- as.vector(fg_seq)
+    
+    # Create background GRange object (+/- X bp of end)
+    # to study RBS nucleotide frequence for entries on + and - strand
+    pos_grange <- subset(
+        ref_grange, strand == "+" & id %in% ref_expr_list[[i]])
+    ranges(pos_grange) <- IRanges(
+        start = (end(pos_grange) - 30),
+        end = (end(pos_grange) + 2))
+    neg_grange <- subset(
+        ref_grange, strand == "-" & id %in% ref_expr_list[[i]])
+    ranges(neg_grange) <- IRanges(
+        start = (start(neg_grange) - 2),
+        end = (start(neg_grange) + 30))
+    
+    # Concatenate the entries from positive and negative strand
+    bg_grange <- c(pos_grange, neg_grange)
+    names(bg_grange) <- sub("^", "bg_", bg_grange$id)
+    
+    # Get the nucleotide sequence associated with the grange object
+    bg_seq <- getSeq(x = bsu, names = bg_grange)
+    bg_seq_list <- as.vector(bg_seq)
+    
+    # Loop through nucleotide length (for RBS motif)
+    tmp_rbs_motifs <- list()
+    tmp_rbs_motifs_score <- c()
+    for (x in c(6:12)) {
+        
+        # Find the motif between foreground and background
+        tmp <- findMotifFgBg(
+            fg.seq = fg_seq, bg.seq = bg_seq,
+            start.width = x, both.strand = FALSE, flank = 1,
+            max.width = 16, enriched.only = TRUE)
+        
+        # Compile all motifs data if existing
+        if (length(tmp$motifs) != 0) {
+            tmp_rbs_motifs[paste0("length_", x)] <- list(tmp)
+            for (y in names(tmp$motifs)) {
+                tmp_rbs_motifs_score <- c(
+                    tmp_rbs_motifs_score, tmp$motifs[[y]]@score)
+            }
+        }
+        
+    }
+    
+    # Create dataframe from the motif list
+    tmp_rbs_motifs_score %<>%
+        ldply(., "data.frame") %>%
+        set_colnames(c("Motifs", "Score"))
+    
+    # Draw consensus logo sequence of RBS for expressed protein
+    ggplot() +
+        geom_logo(
+            data = fg_seq_list,
+            method = "bits",
+            seq_type = "dna") +
+        theme_logo() +
+        ggtitle(paste(names(ref_expr_list)[i], "proteins")) +
+        scale_x_continuous(
+            name = "Nucleotide position prior CDS",
+            breaks = c(seq(from = 1, to = 33, by = 1)),
+            labels = c(seq(from = -30, to = 2, by = 1))) +
+        annotation_custom(
+            grob = tableGrob(
+                d = tmp_rbs_motifs_score %>%
+                    dplyr::arrange(., desc(Score)) %>%
+                    dplyr::slice(., 1:10),
+                theme = ttheme_minimal(base_size = 8), rows = NULL),
+            xmin = 1, xmax = 10, ymin = 0.5, ymax = 2)
+    
+    # Compile all RBS data into list
+    rbs_motifs[names(ref_expr_list)[i]] <- list(tmp_rbs_motifs)
+    rbs_motifs_score[names(ref_expr_list)[i]] <- list(tmp_rbs_motifs_score)
+    
+}
+
+
+# User provided additional RBS motifs sequence
+user_rbs <- c(
+    "aaaggaggtgt", "agaggtggtgt", "atattaagaggaggag", "agagaacaaggagggg")
+
+
+# Concatenate all RBS motifs sequence in single vector
+all_rbs_motifs <- c(
+    rbs_motifs_score$`Low abundant`$Motifs,
+    rbs_motifs_score$`High abundant`$Motifs) %>%
+    c(., toupper(user_rbs)) %>%
+    unique(.)
+
+# Create the RBS motifs regular expression
+pattern_rbs_motifs <- paste(all_rbs_motifs, collapse = "|") %>%
+    paste0("(", ., ")") %>%
+    paste0(., ".{1,14}(", paste(used_start, collapse = "|"), ")")
+
+# Loop through all target ORFs
+rbs_results <- list() 
+for (x in names(sub_orf_grange)) {
+    
+    # Get only current ORF genomic position
+    tmp_grange <- subset(sub_orf_grange, id == x)
+    
+    # Check if multiple entry with same name
+    if (length(tmp_grange) == 1) {
+        
+        # Include 30 nucleotides prior to start codon in grange
+        if (as.character(strand(tmp_grange)) == "+") {
+            ranges(tmp_grange) <- IRanges(
+                start = (start(tmp_grange) - 30),
+                end = (end(tmp_grange)))
+        } else if (as.character(strand(tmp_grange)) == "-") {
+            ranges(tmp_grange) <- IRanges(
+                start = (start(tmp_grange)),
+                end = (end(tmp_grange) + 30))
+        } else {
+            stop("Strand unknown!")
+        }
+        names(tmp_grange) <- x
+        
+        # Get sequence for updated grange
+        tmp_seq <- getSeq(x = bsu, names = tmp_grange)
+        
+        # Attempt to locate pattern within the sequence
+        tmp <- tmp_seq %>%
+            str_locate_all(
+                string = .,
+                pattern = pattern_rbs_motifs)
+        
+        # Check whether locations were obtained
+        if (
+            length(tmp[[1]][, "start"]) == 0 |
+            length(tmp[[1]][, "end"]) == 0) {
+            
+            # Add explicitely NA if there is no match
+            tmp <- base::data.frame(
+                id = names(tmp_seq),
+                start = NA_integer_, end = NA_integer_,
+                seq = NA_character_,
+                stringsAsFactors = FALSE)
+            
+        } else {
+            
+            # Format as dataframe the current peptide positions
+            tmp2 <- tmp %>%
+                set_names(x) %>%
+                ldply(., "data.frame", .id = "id") %>%
+                dplyr::rowwise(.) %>%
+                dplyr::mutate(
+                    .,
+                    seq = substring(
+                        text = tmp_seq,
+                        first = start,
+                        last = end))
+            
+        }
+        
+        # Compile all RBS search result in list
+        rbs_results[x] <- list(tmp)
+        
+    } else {
+        
+        warning("Multiple entry with same name, these will be ignored!")
+        
+    }
+    
+}
+
+# Transform list into dataframe
+rbs_results %<>%
+    plyr::ldply(., "data.frame", .id = "id")
 
 
 
