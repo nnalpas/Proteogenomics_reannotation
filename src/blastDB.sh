9829606872dc50cb8ff2cf9e6534348ec1b26aa9
nnalpas <nalpas.nicolas@gmail.com>
2019-11-19 08:13:44 +0100

    Use for loop to tar uncompress file

--- src/blastDB.sh
+++ src/blastDB.sh
@@ -6,14 +6,16 @@ cd ${PBS_O_WORKDIR}
 # Load the required modules
 module load blast+/2.6.0
 
+cd ${PBS_O_INITDIR}/BlastDB
 
 # Download NCBI reference protein
 wget "ftp://ftp.ncbi.nlm.nih.gov/blast/db/refseq_protein.*.tar.gz"
-tar -xzvf refseq_protein.*.tar.gz
+for file in `ls refseq_protein.*.tar.gz`; do
+	tar -xzvf  $file
+done
 
 
 # Download uniprot databases to use in blast
-cd ${PBS_O_INITDIR}/BlastDB
 wget "ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/complete/uniprot_trembl.fasta.gz"
 gunzip ./*.gz
 cat *.fasta > uniprot_sprot_trembl.fasta
