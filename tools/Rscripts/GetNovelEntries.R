ff8caec1e9728315f65f758bce56a3387a5a6738
nnalpas <nalpas.nicolas@gmail.com>
2019-11-22 13:52:52 +0100

    Implement the PCTshiny parallelised peptide identification per database (it is faster and more accurate)

--- tools/Rscripts/GetNovelEntries.R
+++ tools/Rscripts/GetNovelEntries.R
@@ -41,24 +41,25 @@ if (interactive()) {
 # Load the required packages (or install if not already in library)
 # Note: the select function from dplyr and UniProt.ws is overlapping,
 # therefore order of package loading is important
-load_package("plyr")
-load_package("dplyr")
-load_package("seqinr")
-load_package("bit64")
-load_package("magrittr")
-load_package("data.table")
-load_package("splitstackshape")
-load_package("stringr")
-load_package("optparse")
-load_package("ggplot2")
-load_package("gtable")
-load_package("grid")
-load_package("gridExtra")
-load_package("purrr")
-load_package("foreach")
-load_package("doParallel")
-registerDoParallel(cores = 1)
-print(paste("Number of threads registered:", getDoParWorkers()))
+library(plyr)
+library(dplyr)
+library(seqinr)
+library(bit64)
+library(magrittr)
+library(data.table)
+library(splitstackshape)
+library(stringr)
+library(optparse)
+library(ggplot2)
+library(gtable)
+library(ggpubr)
+library(plotly)
+library(grid)
+library(gridExtra)
+library(purrr)
+library(foreach)
+library(doParallel)
+library(shiny)
 
 
 
@@ -78,7 +79,8 @@ if (interactive()) {
             multi = FALSE),
         novel = choose.files(
             caption = "Choose fasta file containing ORF proteins!",
-            multi = FALSE))
+            multi = FALSE),
+        threads = readline(prompt = "How many cores to use?") %>% as.integer())
     
 } else {
     
@@ -99,7 +101,12 @@ if (interactive()) {
         make_option(
             opt_str = c("-n", "--novel"),
             type = "character", default = NULL,
-            help = "ORF fasta file", metavar = "character"))
+            help = "ORF fasta file", metavar = "character"),
+        make_option(
+            opt_str = c("-t", "--threads"),
+            type = "integer", default = "",
+            help = "Number of cores to use",
+            metavar = "character"))
     
     # Parse the parameters provided on command line by user
     opt_parser <- OptionParser(option_list = option_list)
@@ -116,6 +123,17 @@ if (
     stop("All arguments must be supplied!")
     
 }
+if (
+    identical(opt$threads, NULL) |
+    identical(opt$threads, "") |
+    identical(opt$threads, integer(0))) {
+    
+    warning("Default number of threads will be used!")
+    opt$threads <- 1
+    
+}
+registerDoParallel(cores = opt$threads)
+print(paste("Number of threads registered:", getDoParWorkers()))
 
 # Create output directory if not already existing
 dir.create(opt$output)
@@ -137,6 +155,12 @@ warning(paste(
     "PSM with no MS/MS (matching), these will be ignored!"))
 evid %<>%
     dplyr::filter(., `MS/MS count` >= 1)
+
+# Import the maxquant peptide table
+pep <- mq_read(
+    path = opt$maxquant,
+    name = "peptides.txt",
+    integer64 = "double")
 
 # Import the maxquant proteingroups table
 pg <- mq_read(
@@ -165,68 +189,108 @@ names(fasta$Known) %<>%
 
 ### Novel peptide identification -----------------------------------------
 
-start_time <- Sys.time()
+# Define most represented cleave sites and the enzymatic rule
+digest_rule <- digestion_rule(
+    x = pep[["Amino acid before"]])
 
+# Define missed cleavages rule
+mc <- c(min(
+    pep[["Missed cleavages"]]):max(
+        pep[["Missed cleavages"]]))
 
-# Format association of peptide to protein
-data <- evid %>%
-    dplyr::select(., Sequence, Proteins) %>%
-    cSplit(indt = ., splitCols = "Proteins", sep = ";", direction = "long") %>%
-    unique(.) %>%
-    base::as.data.frame(., stringsAsFactors = FALSE)
+# Digest all proteins into peptide to get their location and
+# their database of origin
+digest_db <- prot_digest_foreach(
+    databases = fasta,
+    custom = digest_rule, mc = mc)
 
-# Locate all the peptide within associated proteins
-pep_loc <- list()
-pep_loc[["Known"]] <- pept_locate(
-    data = data, peptide = "Sequence",
-    proteins = "Proteins", fasta = fasta$Known) %>%
-    dplyr::filter(., !is.na(start))
-pep_loc[["Novel"]] <- pept_locate(
-    data = data, peptide = "Sequence",
-    proteins = "Proteins", fasta = fasta$Novel) %>%
-    dplyr::filter(., !is.na(start))
-pep_loc[["Contaminant"]] <- data %>%
-    dplyr::filter(., grepl("^CON__", Proteins)) %>%
-    set_colnames(c("pep", "prot")) %>%
-    dplyr::mutate(., start = NA_integer_, end = NA_integer_)
-pep_loc[["Reverse"]] <- evid %>%
-    dplyr::filter(., grepl("^REV__", `Leading proteins`) | Reverse == "+") %>%
-    dplyr::select(., Sequence, Proteins) %>%
-    set_colnames(c("pep", "prot")) %>%
-    dplyr::mutate(., start = NA_integer_, end = NA_integer_)
+# Get all identified protein IDs
+prot_ids <- evid %>%
+    strsplit(x = .[["Proteins"]], split = ";", fixed = TRUE) %>%
+    unlist(.) %>%
+    grep("CON__|REV__", ., invert = TRUE, value = TRUE) %>%
+    unique(.)
+
+# Check whether all protein IDs can be found
+if (all(!prot_ids %in% digest_db$Proteins)) {
+    stop("ID clean-up failed, check peptide and digested table!")
+} else {
+    warning(paste0(
+        "Not all protein IDs are represented in digested table",
+        " files provided, ",
+        length(which(
+            !prot_ids %in% digest_db$Proteins)),
+        " IDs are missing!"))
+}
+
+# Define all peptide sequence identified and include peptide
+# that lost the first methionine
+evid <- pep %>%
+    dplyr::select(., Sequence, `Amino acid before`) %>%
+    dplyr::left_join(x = evid, y = ., by = "Sequence")
+
+# List all peptide sequence (including sequence with aa before)
+all_possible_pep <- unique(c(
+    evid[["Sequence"]],
+    paste0(
+        evid[["Amino acid before"]],
+        evid[["Sequence"]])))
+
+# Digest all proteins into peptide to get their location and
+# their database of origin
+digest_datab <- unique_to_database(
+    digest = digest_db,
+    pep = all_possible_pep)
+
+# Clean up the environment of most memory hungry variables
+# should be uncommented in case of RAM run out
+#rm(digest_db)
+
+# Retrieve the peptide location for those peptide that are missing
+# the amino acid before (typically a methionine)
+missing_pep <- evid %>%
+    dplyr::filter(., !Sequence %in% digest_datab$Sequence) %>%
+    mq_rev_con_filt(.) %>%
+    dplyr::mutate(
+        ., DigestSeq = paste0(`Amino acid before`, Sequence)) %>%
+    dplyr::select(., Sequence, DigestSeq) %>%
+    dplyr::left_join(
+        x = ., y = digest_datab,
+        by = c("DigestSeq" = "Sequence")) %>%
+    dplyr::filter(., !is.na(id)) %>%
+    dplyr::mutate(., start = start + 1) %>%
+    dplyr::select(., -DigestSeq)
+pep_pos <- dplyr::bind_rows(digest_datab, missing_pep) %>%
+    dplyr::select(., -id) %>%
+    dplyr::filter(
+        ., Sequence %in% evid[["Sequence"]])
+
+# Compile peptide type info for each peptide sequence
+pep_comp <- pep_pos %>%
+    dplyr::group_by(., Sequence) %>%
+    dplyr::summarise(
+        ., DatabID = paste(sort(unique(Dbuniqueness)), collapse = "-"))
 
 # New dataframe to hold info about fasta of origin for each sequence
-evid_match <- plyr::ldply(
-    .data = pep_loc, .fun = "data.frame", .id = "DatabID") %>%
-    dplyr::group_by(., pep) %>%
-    dplyr::summarise(
-        ., 
-        group = ifelse(
-            test = any(DatabID == "Known"),
-            yes = "Known",
-            no = ifelse(
-                test = any(DatabID == "Contaminant"),
-                yes = "Contaminant",
-                no = ifelse(
-                    test = any(DatabID == "Novel"),
-                    yes = "Novel",
-                    no = ifelse(
-                        test = any(DatabID == "Reverse"),
-                        yes = "Reverse",
-                        no = NA_character_)))),
-        Database = ifelse(
-            test = group %in% c("Known", "Contaminant"),
-            yes = "Target",
-            no = ifelse(
-                test = group == "Reverse",
-                yes = "Decoy",
-                no = ifelse(
-                    test = group == "Novel",
-                    yes = "Novel",
-                    no = NA_character_)))) %>%
-    dplyr::left_join(x = evid, y = ., by = c("Sequence" = "pep")) %>%
+evid_match <- evid %>%
+    dplyr::left_join(x = ., y = pep_comp, by = "Sequence") %>%
+    dplyr::ungroup(.) %>%
     dplyr::mutate(
-    ., OnlyIdBySite = ifelse(id %in% pg_not_sites, TRUE, FALSE))
+        ., 
+        group = dplyr::case_when(
+            grepl("Known", DatabID) ~ "Known",
+            grepl("CON__", Proteins) ~ "Contaminant",
+            grepl("Novel", DatabID) ~ "Novel",
+            grepl("REV__", Proteins) |
+                grepl("REV__", `Leading proteins`) ~ "Reverse",
+            TRUE ~ NA_character_
+        ),
+        Database = dplyr::case_when(
+            group %in% c("Known", "Contaminant") ~ "Target",
+            group == "Reverse" ~ "Decoy",
+            group == "Novel" ~ "Novel",
+            TRUE ~ NA_character_
+        ))
 
 # Print warning for unidentified sequence origin
 print(paste(
@@ -237,9 +301,6 @@ print(paste(
 print(table(evid_match$group, useNA = "always"))
 
 
-start_time - Sys.time()
-
-
 
 ### Results export and visualisation -------------------------------------
 
@@ -248,10 +309,38 @@ pdf(
     file = paste0(opt$output, "/", "GetNovelEntries.pdf"),
     width = 10, height = 10)
 
+# Define colour coding for consistency across plots
+group_colours <- c(
+    `Known` = "#2180FC", `Contaminant` = "#fc9a21",
+    `Reverse` = "#ABA9A9", `Novel` = "#F23D3D")
+database_colours <- c(
+    `Target` = "#2180FC", `Decoy` = "#ABA9A9", `Novel` = "#F23D3D")
+
 # Histogram of evidence counts
-toplot <- evid_match %>%
+# Histogram of evidence counts
+toplot <- evid_match %>% #params[["evid_match"]] %>%
     dplyr::group_by(., group) %>%
     dplyr::summarise(., evid_count = n())
+toplot$group <- factor(
+    x = toplot$group, levels = names(group_colours), ordered = TRUE)
+pl <- ggplot(
+    toplot,
+    aes(
+        x = group, y = evid_count, fill = group,
+        colour = group, label = evid_count)) +
+    geom_bar(stat = "identity", position = "dodge", alpha = 0.7) +
+    geom_text(vjust = -0.5) +
+    xlab("Group") +
+    ylab("Count") +
+    labs(fill = "Group") +
+    theme_pubr() +
+    scale_y_log10() +
+    scale_fill_manual(values = group_colours) +
+    scale_colour_manual(guide = FALSE, values = group_colours)
+
+
+
+
 pl <- plots_hist(
     data = toplot,
     key = "group",
@@ -423,15 +512,28 @@ plot(pl[[1]])
 # Close the picture file
 dev.off()
 
+
+
+### Generate the report --------------------------------------------------
+
+# Generate the markdown report
+report_markdown(
+    rmd_file = "GetNovelEntries.rmd",
+    params = list(
+        fastas = fasta,
+        evid_match = evid_match),
+    format = "html_document",
+    ext = "html")
+
 # Save the group mapping data
 saveRDS(
     object = evid_match,
     file = paste(
-        opt$output, "/", "Sequence_group_mapping.RDS", sep = ""))
+        opt$output, "/", "Group_evidence.RDS", sep = ""))
 
 # Save the peptide location data
 saveRDS(
-    object = pep_loc,
+    object = pep_pos,
     file = paste(
         opt$output, "/", "Peptides_location.RDS", sep = ""))
 
