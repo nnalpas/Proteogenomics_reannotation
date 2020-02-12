e4c73c624f309950cef6aa8703a137918aeb3c30
nnalpas <nalpas.nicolas@gmail.com>
2020-02-12 09:47:03 +0100

    Also include the function annotation into each best blast results

--- tools/Rscripts/blastdbcmd_annotation.R
+++ tools/Rscripts/blastdbcmd_annotation.R
@@ -1,6 +1,11 @@
 
 
 
+### Working environment set-up -------------------------------------------
+
+#
+rm(list = ls())
+
 #
 library(dplyr)
 library(tidyr)
@@ -8,6 +13,10 @@ library(plyr)
 library(magrittr)
 library(splitstackshape)
 library(data.table)
+
+
+
+### Import the data ------------------------------------------------------
 
 #
 my_uni <- fread(
@@ -30,6 +39,26 @@ my_strath <- fread(
     sep = ";", quote = "",
     header = FALSE, stringsAsFactors = FALSE, data.table = FALSE)
 
+my_best_uni <- fread(
+    input = "C:\\Users\\kxmna01\\Dropbox\\Home_work_sync\\Work\\Srim_6frame\\Blast\\Best_blast_ORFprot_vs_Uniprot",
+    sep = "\t", quote = "",
+    header = TRUE, stringsAsFactors = FALSE, data.table = FALSE)
+
+my_best_ref <- fread(
+    input = "C:\\Users\\kxmna01\\Dropbox\\Home_work_sync\\Work\\Srim_6frame\\Blast\\Best_blast_ORFprot_vs_Refprot",
+    sep = "\t", quote = "",
+    header = TRUE, stringsAsFactors = FALSE, data.table = FALSE)
+
+my_best_ncbi <- fread(
+    input = "C:\\Users\\kxmna01\\Dropbox\\Home_work_sync\\Work\\Srim_6frame\\Blast\\Best_blast_ORFprot_vs_NCBIprot",
+    sep = "\t", quote = "",
+    header = TRUE, stringsAsFactors = FALSE, data.table = FALSE)
+
+my_best_strath <- fread(
+    input = "C:\\Users\\kxmna01\\Dropbox\\Home_work_sync\\Work\\Srim_6frame\\Blast\\Best_blast_ORFprot_vs_Strathprot",
+    sep = "\t", quote = "",
+    header = TRUE, stringsAsFactors = FALSE, data.table = FALSE)
+
 my_best_reci_uni <- fread(
     input = "C:\\Users\\kxmna01\\Dropbox\\Home_work_sync\\Work\\Srim_6frame\\ReciprocalBlast\\Best_Reciproc_Blast_Uniprot_vs_ORFprot",
     sep = "\t", quote = "",
@@ -49,6 +78,10 @@ my_best_reci_strath <- fread(
     input = "C:\\Users\\kxmna01\\Dropbox\\Home_work_sync\\Work\\Srim_6frame\\ReciprocalBlast\\Best_Reciproc_Blast_Strathprot_vs_ORFprot",
     sep = "\t", quote = "",
     header = TRUE, stringsAsFactors = FALSE, data.table = FALSE)
+
+
+
+### Format the functional annotation -------------------------------------
 
 #
 my_uni_format <- my_uni %>%
@@ -95,6 +128,55 @@ my_strath_format <- my_strath %>%
             "(^ | $)", "", DescriptionTMP),
         Taxon = "Streptomyces rimosus subsp. rimosus ATCC 10970") %>%
     dplyr::select(., ID, Description, Taxon, TaxonID)
+
+
+
+### Annotation for best hits ---------------------------------------------
+
+#
+my_best_uni %<>%
+    dplyr::mutate(., sseqid = sub(".+\\|(.+?)\\|.+", "\\1", sseqid)) %>%
+    dplyr::left_join(x = ., y = my_uni_format, by = c("sseqid" = "ID"))
+
+my_best_ref %<>%
+    dplyr::mutate(., sseqid = sub(".+\\|(.+?)\\|.+", "\\1", sseqid)) %>%
+    dplyr::left_join(x = ., y = my_ref_format, by = c("sseqid" = "ID"))
+
+my_best_ncbi %<>%
+    dplyr::mutate(., sseqid = sub("\\|$", "", sseqid) %>% sub(".+\\|", "", .)) %>%
+    dplyr::left_join(x = ., y = my_ncbi_format, by = c("sseqid" = "ID"))
+
+my_best_strath %<>%
+    dplyr::left_join(x = ., y = my_strath_format, by = c("sseqid" = "ID"))
+
+#
+data.table::fwrite(
+    x = my_best_uni,
+    file = "C:\\Users\\kxmna01\\Dropbox\\Home_work_sync\\Work\\Srim_6frame\\Blast\\Best_blast_ORFprot_vs_Uniprot_annot",
+    append = FALSE, quote = FALSE, sep = "\t",
+    row.names = FALSE, col.names = TRUE)
+
+data.table::fwrite(
+    x = my_best_ref,
+    file = "C:\\Users\\kxmna01\\Dropbox\\Home_work_sync\\Work\\Srim_6frame\\Blast\\Best_blast_ORFprot_vs_Refprot_annot",
+    append = FALSE, quote = FALSE, sep = "\t",
+    row.names = FALSE, col.names = TRUE)
+
+data.table::fwrite(
+    x = my_best_ncbi,
+    file = "C:\\Users\\kxmna01\\Dropbox\\Home_work_sync\\Work\\Srim_6frame\\Blast\\Best_blast_ORFprot_vs_NCBIprot_annot",
+    append = FALSE, quote = FALSE, sep = "\t",
+    row.names = FALSE, col.names = TRUE)
+
+data.table::fwrite(
+    x = my_best_strath,
+    file = "C:\\Users\\kxmna01\\Dropbox\\Home_work_sync\\Work\\Srim_6frame\\Blast\\Best_blast_ORFprot_vs_Strathprot_annot",
+    append = FALSE, quote = FALSE, sep = "\t",
+    row.names = FALSE, col.names = TRUE)
+
+
+
+### Annotation for reciprocal best hits ----------------------------------
 
 #
 my_best_reci_uni %<>%
