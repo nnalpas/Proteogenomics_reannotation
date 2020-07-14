9cecb40adadbdb18a41313f652249cd0c8f476f1
nnalpas <nalpas.nicolas@gmail.com>
2020-07-14 17:39:50 +0200

    Python script to extract the cds AA and genome nucleotide sequences from Genbank file into fasta files

---
+++ tools/Python/Genbank_to_fasta.py
@@ -0,0 +1,45 @@
+
+
+
+from Bio import SeqIO
+gbk_filename = "H:\\Cyanobacteria\\genbank\\pSYSM.genbank"
+faa_filename = "H:\\Cyanobacteria\\genbank\\pSYSM_cds_aa.fasta"
+input_handle  = open(gbk_filename, "r")
+output_handle = open(faa_filename, "w")
+
+for seq_record in SeqIO.parse(input_handle, "genbank") :
+    print("Dealing with GenBank record %s" % seq_record.id)
+    for seq_feature in seq_record.features :
+        if seq_feature.type=="CDS" :
+            if 'translation' in seq_feature.qualifiers :
+                output_handle.write(">%s | %s | %s\n%s\n" % (
+                    seq_feature.qualifiers['gene'][0],
+                    #seq_feature.qualifiers['locus_tag'][0],
+                    #seq_feature.qualifiers['label'][0],
+                    seq_record.name,
+                    #seq_feature.qualifiers['product'][0],
+                    seq_feature.location,
+                    seq_feature.qualifiers['translation'][0]))
+            else :
+                print("Translation qualifier not found %s" % seq_feature.qualifiers['locus_tag'][0])
+
+output_handle.close()
+input_handle.close()
+
+
+
+from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+from Bio.Alphabet import generic_dna
+gbk_filename = "H:\\Cyanobacteria\\genbank\\pSYSX.genbank"
+faa_filename = "H:\\Cyanobacteria\\genbank\\pSYSX.fasta"
+input_handle  = open(gbk_filename, "r")
+
+for seq_record in SeqIO.parse(input_handle, "genbank") :
+    print("Dealing with GenBank record %s" % seq_record.id)
+    SeqIO.write(seq_record, faa_filename, "fasta")
+
+input_handle.close()
+
+
