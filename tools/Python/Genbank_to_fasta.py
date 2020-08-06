2e8cea78abdb8f79ab92b9c92cbdfa6998fcbf8f
nnalpas <nalpas.nicolas@gmail.com>
2020-08-06 17:00:16 +0200

    Extract all gene nucleotide sequences from genbank files

--- tools/Python/Genbank_to_fasta.py
+++ tools/Python/Genbank_to_fasta.py
@@ -1,6 +1,6 @@
 
 
-
+# Extract CDS amino acid sequences
 from Bio import SeqIO
 gbk_filename = "H:\\Cyanobacteria\\genbank\\pSYSM.genbank"
 faa_filename = "H:\\Cyanobacteria\\genbank\\pSYSM_cds_aa.fasta"
@@ -27,7 +27,7 @@ output_handle.close()
 input_handle.close()
 
 
-
+# Extract genomic sequence
 from Bio import SeqIO
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
@@ -40,6 +40,28 @@ for seq_record in SeqIO.parse(input_handle, "genbank") :
     print("Dealing with GenBank record %s" % seq_record.id)
     SeqIO.write(seq_record, faa_filename, "fasta")
 
+input_handle.close()
+
+
+# Extract gene nucleotide sequences
+from Bio import SeqIO
+gbk_filename = "H:\\data\\Synechocystis_6frame\\genbank\\pSYSX.genbank"
+faa_filename = "H:\\data\\Synechocystis_6frame\\genbank\\pSYSX_gene_nucl.fasta"
+input_handle  = open(gbk_filename, "r")
+output_handle = open(faa_filename, "w")
+
+for seq_record in SeqIO.parse(input_handle, "genbank") :
+    print("Dealing with GenBank record %s" % seq_record.id)
+    for seq_feature in seq_record.features :
+        if seq_feature.type=="gene" :
+            output_handle.write(">%s | %s | %s\n%s\n" % (
+                seq_feature.qualifiers['gene'][0],
+                #seq_feature.qualifiers['locus_tag'][0],
+                seq_record.name,
+                seq_feature.location,
+                seq_feature.location.extract(seq_record).seq))
+
+output_handle.close()
 input_handle.close()
 
 
