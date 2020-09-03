f2f95b63a0e401da79df12b7478b503dabed00e9
nnalpas <nalpas.nicolas@gmail.com>
2020-09-03 08:48:57 +0200

    Use external (user-specified) parameter variables for the protein annotation step

--- src/SixFrame_proteogenomics.sh
+++ src/SixFrame_proteogenomics.sh
@@ -264,13 +264,14 @@ if [ $ProteinAnnotation == 1 ]; then
     ${PBS_O_HOME}/bin/Fasta_annotation.R \
 		-f ${UNIREFPROT} \
 		-t ${TaxId} \
-		-c "ENTRY-NAME,GENES,PROTEIN-NAMES,SUBCELLULAR-LOCATIONS,FAMILIES" \
-		-k "UNIPROTKB" \
+		-c ${AnnotColumn} \
+		-k ${AnnotKey} \
+		-s ${AnnotSeparator} \
 		-o ${ProjDir}/ProtAnnotation/Ref_prot_annotations.txt > ${LogDir}/ProteinAnnotation.log 2>&1
 	${PBS_O_HOME}/bin/Transfer_annotation.R \
 		-c ${ProjDir}/ReciprocalBlast/Best_Reciproc_Blast_cross-map_Refprot_vs_ORFprot \
 		-a ${ProjDir}/ProtAnnotation/Ref_prot_annotations.txt \
-		-k "UNIPROTKB" \
+		-k ${AnnotKey} \
 		-r "sseqid" \
 		-o ${ProjDir}/ProtAnnotation/Orf_prot_annotations.txt >> ${LogDir}/ProteinAnnotation.log 2>&1
 	
