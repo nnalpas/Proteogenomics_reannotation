e2af1cc6555eeb6be629b6f5f9ccd01187336752
nnalpas <nalpas.nicolas@gmail.com>
2021-01-21 15:38:17 +0100

    It is now possible to provide a grange instead of maxquant table in order to parse phenodata info

--- tools/Rscripts/SummarizedExp_MQ_preparation.R
+++ tools/Rscripts/SummarizedExp_MQ_preparation.R
@@ -52,10 +52,13 @@ if (interactive()) {
     # Define the list of input parameters
     opt <- list(
         input = choose.files(
-            caption = "Choose input MaxQuant txt file!",
+            caption = "Choose input MaxQuant txt file or GRange RDS file!",
             multi = FALSE),
         prefix = readline(
             prompt = "Define a prefix for the output filenames!"),
+        grange = readline(
+            prompt = "The input is a GRange object (logical)!") %>%
+            as.logical(.),
         output = readline(
             prompt = "Define the output folder!"))
     
@@ -66,12 +69,17 @@ if (interactive()) {
         make_option(
             opt_str = c("-i", "--input"),
             type = "character", default = "",
-            help = "Input MaxQuant txt file (containing abundance columns)",
+            help = "Input MaxQuant txt file or GRange RDS file (containing abundance columns)",
             metavar = "character"),
         make_option(
             opt_str = c("-p", "--prefix"),
             type = "character", default = "", 
             help = "Define a prefix for the output filenames",
+            metavar = "character"),
+        make_option(
+            opt_str = c("-g", "--grange"),
+            type = "logical", default = FALSE, 
+            help = "The input is a GRange object [default= %default]",
             metavar = "character"),
         make_option(
             opt_str = c("-o", "--output"),
@@ -92,7 +100,7 @@ if (
     
     print_help(opt_parser)
     stop(paste(
-        "MaxQuant txt file required!"))
+        "MaxQuant txt or GRange RDS file is required!"))
     
 }
 
@@ -121,40 +129,81 @@ what_to_look_for <- list(
     Sites = c("Intensity")
 )
 
+# Which column to use as ID
+what_col_for_id <- list(
+    proteinGroups = c("Protein IDs")
+)
+
 # Find out the file type for the expression table and therefore which
 # columns to retrieve
-my_col <- lapply(names(what_to_look_for), function(i) {
+my_file_type <- lapply(names(what_to_look_for), function(i) {
     if (grepl(i, opt[["input"]])) {
-        what_to_look_for[i]
+        i
     }
 }) %>%
-    unlist(., recursive = FALSE)
+    unlist(.)
 
 # Check that the file type can be accurately defined
-if (length(names(my_col)) > 1) {
+if (length(my_file_type) != 1) {
     stop(paste(
         "The file type cannot be determined, could be either:",
-        paste0(names(my_col), collapse = " or ")))
-} else {
-    my_col %<>%
-        unlist(., use.names = FALSE)
+        paste0(my_file_type, collapse = " or ")))
 }
 
 # Import the expression data
-my_data <- data.table::fread(
-    input = opt[["input"]], sep = "\t", quote = "", header = TRUE,
-    stringsAsFactors = FALSE, colClasses = "character")
+if (opt[["grange"]]) {
+    my_data <- readRDS(opt[["input"]]) %>%
+        values(.)
+} else {
+    my_data <- data.table::fread(
+        input = opt[["input"]], sep = "\t", quote = "", header = TRUE,
+        stringsAsFactors = FALSE, colClasses = "character")
+}
 
 
 
 ### Data formatting ------------------------------------------------------
 
+# What column should be retrieved based on file type
+my_cols <- what_to_look_for[[my_file_type]] %>%
+    unlist(., use.names = FALSE)
+
 # Loop through each column type to retrieve
-my_pheno <- lapply(my_col, function(j) {
+my_pheno <- lapply(my_cols, function(j) {
     required_file_summExp(
         my_data = my_data, my_col = j, input_path = x)
 }) %>%
     plyr::ldply(., "data.table", .id = NULL)
+
+# In case imported file is not a grange object, add a 'id' column
+if (!opt[["grange"]]) {
+    
+    my_data_format <- my_data
+    
+    # Before renaming column name to id, make sure an 'id' column
+    # is not already existing
+    if ("id" %in% colnames(my_data_format)) {
+        
+        my_data_format %<>%
+            dplyr::rename(., id_OLD = id)
+        warning("A column called 'id' was renamed to 'id_OLD'!")
+        
+    }
+    
+    # Define the column used as id
+    my_id <- what_col_for_id[[my_file_type]]
+    
+    # If such column exist then 
+    if (my_id %in% colnames(my_data_format)) {
+        my_data_format %<>%
+            dplyr::mutate(., id = !!as.name(my_id))
+    } else {
+        stop(paste0(
+            "The column: ", my_id,
+            " cannot be used as 'id', as it is missing!"))
+    }
+    
+}
 
 
 
@@ -166,7 +215,7 @@ out_name <- opt[["input"]] %>%
     c(opt[["prefix"]], .) %>%
     paste0(., collapse = "_") %>%
     paste0(opt[["output"]], "/", .) %>%
-    sub("\\.txt", "", .)
+    sub("\\.(txt|RDS)", "", .)
 
 # Write the phenodata to output folder
 data.table::fwrite(
@@ -174,11 +223,18 @@ data.table::fwrite(
     append = FALSE, quote = FALSE, sep = "\t",
     row.names = FALSE, col.names = TRUE)
 
-# Copy the expression data to output folder with matching name
-file.copy(
-    from = opt[["input"]],
-    to = paste0(out_name, "_expr.txt"),
-    overwrite = FALSE)
+# Write the expression data to output folder with matching name
+if (opt[["grange"]]) {
+    file.copy(
+        from = opt[["input"]],
+        to = paste0(out_name, "_expr.RDS"),
+        overwrite = FALSE)
+} else {
+    data.table::fwrite(
+        x = my_data_format, file = paste0(out_name, "_expr.txt"),
+        append = FALSE, quote = FALSE, sep = "\t",
+        row.names = FALSE, col.names = TRUE)
+}
 
 # Define end time
 print(paste("Complete", format(Sys.time(), "%Y-%m-%d %H:%M:%S")))
