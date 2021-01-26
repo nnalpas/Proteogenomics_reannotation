0bbf33ea74ed9761355c5bd5a94ed774857027f7
nnalpas <nalpas.nicolas@gmail.com>
2021-01-26 15:32:20 +0100

    New script to identify all files that should be prepared for import into summarizedExperiment object, input is one or more folders containing proteinGroups.txt or Sites_grange.RDS files

---
+++ src/SummarizedExpPreparation.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+# Script to generate expression and phenodata files from MaxQuant processing folder
+
+# Time scripts starts
+echo "$0"
+echo "Start $(date +"%T %d-%m-%Y")."
+
+# Function holding the usage
+display_usage() { 
+	echo "
+		Usage: $0 [Options] <Input folder 1 (containing proteinGroups.txt or grange.RDS)> <...> <folder N>"
+	echo "
+		Options:
+        	-o	[str]	The output directory.
+        	-h	[]	To display the help.
+	"
+}
+
+# Define default values
+o_default="`pwd`/SummarizedExp"
+
+# Parse user parameters
+while getopts "o:h" opt; do
+	case $opt in
+		o)
+			WKDIR=$OPTARG
+			echo "Working directory: $WKDIR"
+			shift $((OPTIND-1)); OPTIND=1
+			;;
+		h)
+			display_usage
+			shift $((OPTIND-1)); OPTIND=1
+			exit 0
+			;;
+		\?)
+			display_usage
+			exit 1
+			;;
+		:)
+			display_usage
+			exit 1
+			;;
+	esac
+done
+
+# Also get target sequence location files from parameter
+INPUTS=${@:1}
+
+# Setting default values ${VARIABLE=DEFAULT_VALUE}
+# This command will set VARIABLE to DEFAULT_VALUE if it is currently 
+# undefined, then return VARIABLE
+: ${WKDIR=$o_default}
+
+# Check for mandatory parameters
+if [ -z "${INPUTS}" ]; then
+	echo "At least one input folder (containing proteinGroups.txt or grange.RDS) must be specified." >&2
+	display_usage
+	exit 1
+fi
+
+# Create the output folder
+if [ ! -d ${WKDIR} ] ; then
+	mkdir ${WKDIR}
+fi
+
+# Loop through all proteinGroups or sites files
+find ${INPUTS[@]} -type f \( -name "*proteinGroups.txt" -o -name "*Sites_grange.RDS" \) -print0 | 
+while IFS= read -r -d '' file; do
+    
+	ext=`basename "$file" | sed 's/.+\.//'`
+	prefix=`basename "$file" | sed 's/(.+)_.+/\1/'`
+	isgrange="FALSE"
+	
+	if [[ "${ext}" == "RDS" ]]; then
+		isgrange="TRUE"
+	fi
+	
+	SummarizedExp_MQ_preparation.R \
+		-i "${file}" \
+		-p "${prefix}" \
+		-g "${isgrange}" \
+		-o ${WKDIR}
+	
+done
+
+# Time scripts ends
+echo "Completed $(date +"%T %d-%m-%Y")."
