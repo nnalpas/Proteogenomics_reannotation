e6589fcc60632d9ef23aa9310349466cfb2009a7
nnalpas <nalpas.nicolas@gmail.com>
2021-03-15 17:37:19 +0100

    Update processing parameters

--- tools/Rscripts/tmp_pairwise_genome_comparison.R
+++ tools/Rscripts/tmp_pairwise_genome_comparison.R
@@ -5,7 +5,7 @@
 library(magrittr)
 
 my_data <- data.table::fread(
-    input = "C:/Users/kxmna01/Desktop/Conservation/IPPAS_B-1465_vs_ASM34078v1_for_plotting",
+    input = "C:/Users/kxmna01/Desktop/Conservation/elongatus_vs_ASM34078v1_for_plotting",
     sep = "\t", quote = "", header = TRUE, stringsAsFactors = FALSE)
 
 plot(my_data, type = "l")
