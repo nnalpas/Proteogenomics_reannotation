ff8bb11d0c8bbac0fb9d397c6b707c53d2193bd4
nnalpas <nalpas.nicolas@gmail.com>
2021-03-15 17:36:33 +0100

    Plot the genome synteny (alignment from lastz)

--- tools/Rscripts/tmp_pairwise_genome_comparison.R
+++ tools/Rscripts/tmp_pairwise_genome_comparison.R
@@ -5,17 +5,10 @@
 library(magrittr)
 
 my_data <- data.table::fread(
-    input = "C:/Users/kxmna01/Desktop/lastz_test/bovis_vs_bcg_for_plotting",
+    input = "C:/Users/kxmna01/Desktop/Conservation/IPPAS_B-1465_vs_ASM34078v1_for_plotting",
     sep = "\t", quote = "", header = TRUE, stringsAsFactors = FALSE)
 
 plot(my_data, type = "l")
 
-c("#033A61", "#035416", "#6B0705", "#945725")
-
-my_data_dup <- data.table::fread(
-    input = "H:/data/Synechocystis_6frame/Conservation/Synechocystis_sp_PCC_6803_vs_itself_for_plotting",
-    sep = "\t", quote = "", header = TRUE, stringsAsFactors = FALSE)
-
-plot(my_data_dup, type = "l")
 
 
