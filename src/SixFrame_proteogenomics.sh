c790c563418bf7665572c2e03eadf4eb5e59effc
nnalpas <nalpas.nicolas@gmail.com>
2021-10-21 15:07:49 +0200

    Cross-mapping of protein IDs between S. rimosus and S. coelicolor

--- src/SixFrame_proteogenomics.sh
+++ src/SixFrame_proteogenomics.sh
@@ -44,7 +44,7 @@ cd ${PBS_O_WORKDIR}
 #module load curl/7.57.0
 #module load zlib/1.2.11
 module load blast+/2.10.1
-module load math/R/3.5.2-mkl-2018 # the prerequisite GNU is incompatible with blast+ (must find solution)
+#module load math/R/3.5.2-mkl-2018 # the prerequisite GNU is incompatible with blast+ (must find solution)
 #module load clustal_omega/1.2.4
 module load emboss/6.6.0
 module load devel/perl/5.26
@@ -152,8 +152,7 @@ if [ $MakeBlastDbProt == 1 ]; then
 		-i ${InputType} \
 		-y "prot" \
 		-t ${TaxId} \
-		${UNIREFPROT} \
-		${SIXFRAMEPROT} ${OTHERPROT[@]} > ${LogDir}/MakeBlastDb.log 2>&1
+		${PBS_O_HOME}/work/Srim_6frame/CrossMap_Scoelicolor/Streptomyces_coelicolor_proteome_FIXED.fasta > ${LogDir}/MakeBlastDb.log 2>&1
     
 fi
 
