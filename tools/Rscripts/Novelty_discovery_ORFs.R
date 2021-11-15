5c0c43bcd9bac76549c57aeb266f0f8af7f5ad7d
nnalpas <nalpas.nicolas@gmail.com>
2021-11-15 17:23:23 +0100

    Will need to fix this part of the script so that it is not manually changed depending on dataset

--- tools/Rscripts/Novelty_discovery_ORFs.R
+++ tools/Rscripts/Novelty_discovery_ORFs.R
@@ -1711,8 +1711,8 @@ for (i in high_qual_targets) {
         orf_gr = orf_grange_expr,
         pep_gr = pep_grange_unique,
         sanger_gr = sanger_grange,
-        #ref_label = "GENE.NAME")
-        ref_label = "PROTEIN.ID")
+        ref_label = "GENE.NAME")
+        #ref_label = "PROTEIN.ID")
     
     # Plot the known entries and 6-frames ORFs tracks
     my_plots <- genomic_vis_data[["plots"]]
