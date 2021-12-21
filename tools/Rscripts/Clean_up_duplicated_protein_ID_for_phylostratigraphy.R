866df61c6784c4f2cf28bbb1f1ff77ab8432c7a6
nnalpas <nalpas.nicolas@gmail.com>
2021-12-21 16:52:37 +0100

    Temporary script to clean up fasta files with duplicated protein IDs (for phylostratigraphy)

---
+++ tools/Rscripts/Clean_up_duplicated_protein_ID_for_phylostratigraphy.R
@@ -0,0 +1,27 @@
+
+f <- "T:/User/Nicolas/Phylostratigraphy/Formatted/1232426.faa"
+f <- "T:/User/Nicolas/Phylostratigraphy/Formatted/224911.faa"
+f <- "T:/User/Nicolas/Phylostratigraphy/Formatted/243277.faa"
+
+my_seqs <- seqinr::read.fasta(
+    file = f, seqtype = "AA",
+    as.string = TRUE, whole.header = TRUE)
+
+table(duplicated(sub(" .+", "", names(my_seqs))))
+table(duplicated(my_seqs))
+names(my_seqs)[duplicated(sub(" .+", "", names(my_seqs)))]
+
+my_seqs_filt <- my_seqs
+names(my_seqs_filt)[duplicated(sub(" .+", "", names(my_seqs_filt)))] %<>%
+    sub("\\.1 ", ".d ", .)
+
+table(duplicated(sub(" .+", "", names(my_seqs_filt))))
+
+#my_seqs_filt <- my_seqs[!duplicated(sub(" .+", "", names(my_seqs)))]
+
+seqinr::write.fasta(
+    sequences = my_seqs_filt, names = names(my_seqs_filt),
+    file.out = f,
+    open = "w", nbchar = 80, as.string = TRUE)
+
+
