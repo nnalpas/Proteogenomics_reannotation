a40b2d6df67dccc5df511377b3a0eaad71e44be2
nnalpas <nalpas.nicolas@gmail.com>
2022-01-07 21:15:11 +0100

    Plot enrichment from all resources

--- tools/Rscripts/Enrichment_heatmap.R
+++ tools/Rscripts/Enrichment_heatmap.R
@@ -10,34 +10,20 @@ my_plots <- list()
 
 #my_oa_f <- "H:/data/Synechocystis_6frame/Phylostratigraphy/Phylostrata_proteins.txt"
 
-my_oa_f <- "H:/data/Synechocystis_6frame/2022-01-04_PCA/ibaq_values.txt.GSEA.txt"
+my_oa_f <- "H:/data/Synechocystis_6frame/2022-01-07_iBAQ/PG_iBAQ.txt.GSEA.txt"
 
 my_oa <- data.table::fread(
     input = my_oa_f, sep = "\t", quote = "",
     header = TRUE, stringsAsFactors = FALSE)
 
 mandatory_path <- my_oa %>%
-    dplyr::filter(., `-log10_padj` > 2 & resource == "KEGG_Pathway_Name") %>%
+    dplyr::filter(., padj <= 0.01) %>%
     .[["pathway"]] %>%
     unique(.)
-
-mandatory_path <- my_oa %>%
-    dplyr::filter(
-        ., resource %in% c(
-            "Pathway", "GOBP Term", "GOCC Term", "GOMF Term",
-            "Panther Pathway", "KEGG_Pathway_Name")) %>%
-    dplyr::group_by(., set) %>%
-    dplyr::arrange(., desc(`-log10_padj`)) %>%
-    dplyr::slice(., 1:10) %>%
-    .[["pathway"]] %>%
-    unique(.) %>%
-    grep("pxd|ScyCode", ., value = TRUE, invert = TRUE)
-#, "pfam_description"
 
 my_oa_final <- my_oa %>%
     dplyr::filter(
         ., pathway %in% mandatory_path &
-            #resource == "KEGG_Pathway_Name" &
             padj <= 0.05) %>%
     dplyr::mutate(., padj_range = dplyr::case_when(
         padj <= 0.001 ~ "<= 0.001",
@@ -67,29 +53,35 @@ my_oa_final$set <- factor(
     levels = unique(my_oa$set),
     ordered = TRUE)
 
-my_palette <- colorRampPalette(c("#387eb8", "#d1d2d4", "#e21e25"))(n = 10)
+#my_palette <- colorRampPalette(c("#387eb8", "#d1d2d4", "#e21e25"))(n = 10)
+my_palette <- grDevices::hcl.colors(n = 10, palette = "Fall")
 
-my_plots[["Heatmap_oa_sig"]] <- ggplot(
-    my_oa_final,
-    aes(x = set, y = pathway, fill = NES, size = padj_range)) +
-    geom_point(colour = "white", shape = 21) +
-    ggpubr::theme_pubr() +
-    theme(
-        legend.position = "right",
-        axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1),
-        panel.grid.major = element_line(colour = "grey", size = 0.5, linetype = "dotted")) +
-    #facet_grid(rows = vars(resource), scales = "free_y", space = "free_y") +
-    scale_size_manual(
-        values = c(
-            `<= 0.001` = 12, `<= 0.01` = 8,
-            `<= 0.05` = 4)) +
-    scale_fill_gradientn(
-        colours = grDevices::hcl.colors(n = 9, palette = "Fall"))
+my_plots <- lapply(unique(my_oa_final$resource), function(x) {
+    ggplot(
+        my_oa_final %>% dplyr::filter(., resource == x),
+        aes(x = set, y = pathway, fill = NES, size = padj_range)) +
+        geom_point(colour = "white", shape = 21) +
+        ggpubr::theme_pubr() +
+        theme(
+            legend.position = "right",
+            axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1),
+            panel.grid.major = element_line(colour = "grey", size = 0.5, linetype = "dotted")) +
+        #facet_grid(rows = vars(resource), scales = "free_y", space = "free_y") +
+        scale_size_manual(
+            values = c(
+                `<= 0.001` = 8, `<= 0.01` = 5,
+                `<= 0.05` = 2)) +
+        scale_fill_gradientn(
+            colours = grDevices::hcl.colors(n = 9, palette = "Fall"),
+            limits = c(min(my_oa_final$NES), max(my_oa_final$NES))) +
+        ggtitle(x)
+}) %>%
+    set_names(unique(my_oa_final$resource))
 
 my_date <- Sys.Date()
 pdf(
-    file = paste0(my_date, "_phylostratigraphy_OA.pdf"),
-    width = 10, height = 20)
+    file = paste0(my_date, "_enrichment_plots.pdf"),
+    width = 10, height = 10)
 my_plots
 dev.off()
 
