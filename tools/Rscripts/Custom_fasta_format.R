016d33a876c5159d148a165dfda240312db2a92c
nnalpas <nalpas.nicolas@gmail.com>
2022-01-26 20:40:26 +0100

    Use user-specified pattern to clean up the ID

--- tools/Rscripts/Custom_fasta_format.R
+++ tools/Rscripts/Custom_fasta_format.R
@@ -3,7 +3,8 @@
 
 library(magrittr)
 
-my_fasta_f <- "H:/data/Srim_6frame_3rd_analysis/Genome/CP048261_CP048262_prot_sequence_FIXED.fasta"
+my_fasta_f <- "H:/data/Pathogens_6frame/Genome/GCF_000006765.1_ASM676v1_cds_from_genomic.fasta"
+id_pattern <- "^lcl\\|NC_002516\\.2_cds_(.+)_.+$"
 
 my_fasta <- seqinr::read.fasta(
     file = my_fasta_f, seqtype = "AA", as.string = TRUE, whole.header = TRUE)
@@ -32,15 +33,11 @@ my_data_format <- my_data %>%
         data = ., col = "Header", into = c("ID", "REST"),
         sep = " ", extra = "merge") %>%
     dplyr::mutate(
-        ., ID = sub(".+(_.+?)$", "AL939104\\1", ID),
+        ., ID = sub(id_pattern, "\\1", ID),
         locus_tag = ifelse(
             grepl("locus_tag", REST),
             sub(".*\\[locus_tag=(.+?)\\].*", "\\1", REST),
             ""),
-        #gene_id = ifelse(
-        #    grepl("GeneID", REST),
-        #    sub(".*GeneID:(.+?)\\].*", "\\1", REST),
-        #    ""),
         gene_id = ifelse(
             grepl("gene", REST),
             sub(".*\\[gene=(.+?)\\].*", "\\1", REST),
@@ -53,18 +50,19 @@ my_data_format <- my_data %>%
             grepl("protein_id", REST),
             sub(".*\\[protein_id=(.+?)\\].*", "\\1", REST),
             ""),
-        #protein_id = ifelse(
-        #    grepl("CP.+_prot", REST),
-        #    sub(".*\\|CP.+_(prot.+?) .*", "\\1", REST),
-        #    ""),
         pseudo = ifelse(
             grepl("pseudo=", REST),
             sub(".*\\[pseudo=(.+?)\\].*", "\\1", REST),
-            "false"))
+            "false"),
+        location = ifelse(
+            grepl("location=", REST),
+            sub(".*\\[location=(.+?)\\].*", "\\1", REST),
+            ""))
 my_data_format$Header <- paste(
     my_data_format$ID, my_data_format$locus_tag,
     my_data_format$gene_id, my_data_format$protein,
     my_data_format$protein_id, my_data_format$pseudo,
+    my_data_format$location,
     sep = " | ")
 
 # Check that IDs are no more than 15 characters long or provide warning
@@ -75,7 +73,7 @@ if (any(nchar(my_data_format$ID) > 14)) {
 seqinr::write.fasta(
     sequences = my_fasta,
     names = my_data_format$Header,
-    file.out = sub("\\.fasta", "_FIXED.fasta", my_fasta_f),
+    file.out = sub("\\.fasta", "_sep.fasta", my_fasta_f),
     open = "w", nbchar = 60, as.string = TRUE)
 
 
