2b5dc030a3940f22461c43e7c22e29df5e055411
nnalpas <nalpas.nicolas@gmail.com>
2022-03-01 10:31:24 +0000

    Update processing parameters

--- tools/Rscripts/Overrepresentation_heatmap.R
+++ tools/Rscripts/Overrepresentation_heatmap.R
@@ -11,12 +11,25 @@ my_plots <- list()
 #my_oa_f <- "H:/data/Synechocystis_6frame/Phylostratigraphy/Phylostrata_proteins.txt"
 
 #my_oa_f <- "H:/data/Synechocystis_6frame/2022-01-07_iBAQ/PG_iBAQ.txt.GSEA.txt"
-
-my_oa_f <- "H:/data/Synechocystis_6frame/2022-02-14_iBAQ/2022-02-14_Scy004_resuscitation_Scy001_iBAQ_norm_t-test_forOA.txt.OA.txt"
+#my_oa_f <- "H:/data/Synechocystis_6frame/2022-02-14_iBAQ/2022-02-14_Scy004_resuscitation_Scy001_iBAQ_norm_t-test_forOA.txt.OA.txt"
+my_oa_f <- "/mnt/storage/kxmna01/data/Synechocystis_6frame/OA_GSEA/2021-12-23_Phylostrata_for_OA.txt.OA.txt"
 
 my_oa <- data.table::fread(
     input = my_oa_f, sep = "\t", quote = "",
     header = TRUE, stringsAsFactors = FALSE)
+
+my_oa$set <- sub("ps (.) ", "ps 0\\1 ", my_oa$set)
+
+my_oa %<>%
+    dplyr::rowwise(.) %>%
+    dplyr::mutate(
+        ., overlap_perc = overlap / size * 100,
+        padj_range = dplyr::case_when(
+            padj <= 0.001 ~ "<= 0.001",
+            padj <= 0.01 ~ "<= 0.01",
+            padj <= 0.05 ~ "<= 0.05",
+            TRUE ~ NA_character_
+        ))
 
 mandatory_path <- my_oa %>%
     dplyr::filter(., padj <= 0.05 & score > 1) %>%
@@ -27,14 +40,10 @@ my_oa_final <- my_oa %>%
     dplyr::filter(
         ., pathway %in% mandatory_path &
             padj <= 0.05) %>%
-    dplyr::mutate(., padj_range = dplyr::case_when(
-        padj <= 0.001 ~ "<= 0.001",
-        padj <= 0.01 ~ "<= 0.01",
-        padj <= 0.05 ~ "<= 0.05",
-        TRUE ~ NA_character_
-    )) %>%
     dplyr::group_by(., pathway) %>%
-    dplyr::mutate(., median = median(score)) %>%
+    dplyr::mutate(
+        ., median = median(score),
+        Significant = ifelse(padj <= 0.05, "*", "")) %>%
     dplyr::ungroup(.) %>%
     dplyr::arrange(., dplyr::desc(median))
 
@@ -52,7 +61,7 @@ my_oa_final$pathway <- factor(
     ordered = TRUE)
 my_oa_final$set <- factor(
     x = my_oa_final$set,
-    levels = unique(my_oa$set),
+    levels = sort(unique(my_oa$set)),
     ordered = TRUE)
 my_oa_final$log_score <- my_oa_final$score %>%
     log10(.)
@@ -81,6 +90,24 @@ my_plots <- lapply(unique(my_oa_final$resource), function(x) {
         ggtitle(x)
 }) %>%
     set_names(unique(my_oa_final$resource))
+
+my_plots <- lapply(unique(my_oa_final$resource), function(x) {
+    ggplot(
+        my_oa_final %>% dplyr::filter(., resource == x),
+        aes(x = set, y = pathway, fill = overlap_perc, label = Significant)) +
+        geom_tile(colour = "white") +
+        geom_text(colour = "white") +
+        ggpubr::theme_pubr() +
+        theme(
+            legend.position = "right",
+            axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1)) +
+        scale_fill_gradientn(
+            colours = grDevices::hcl.colors(n = 9, palette = "Fall"),
+            limits = c(0.1, 100)) +
+        ggtitle(x)
+}) %>%
+    set_names(paste(unique(my_oa_final$resource), "percentage")) %>%
+    c(my_plots, .)
 
 my_date <- Sys.Date()
 pdf(
