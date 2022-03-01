09e7f014d604560c41c030f08ac713bb4de50137
nnalpas <nalpas.nicolas@gmail.com>
2022-03-01 10:33:17 +0000

    Focus on important pathways for phylostratigraphy

--- tools/Rscripts/OA_heatmap.R
+++ tools/Rscripts/OA_heatmap.R
@@ -8,7 +8,7 @@ my_plots <- list()
 
 #my_oa_f <- "H:/data/Synechocystis_6frame/Phylostratigraphy/Phylostrata_proteins.txt"
 
-my_oa_f <- "H:/data/Synechocystis_6frame/OA_GSEA/2021-12-23_Phylostrata_for_OA.txt.OA.txt"
+my_oa_f <- "/mnt/storage/kxmna01/data/Synechocystis_6frame/OA_GSEA/2021-12-23_Phylostrata_for_OA.txt.OA.txt"
 
 my_oa <- data.table::fread(
     input = my_oa_f, sep = "\t", quote = "",
@@ -19,12 +19,18 @@ my_oa %<>%
     dplyr::mutate(., overlap_perc = overlap / size * 100)
 
 mandatory_path <- my_oa[
-    my_oa$resource == "Custom_annotation",][["pathway"]] %>%
+    my_oa$resource %in% c("Miscellaneous", "best_og_Subcategory", "Characterization"),][["pathway"]] %>%
     unique(.) %>%
     grep("pxd|ScyCode", ., value = TRUE, invert = TRUE)
 mandatory_path <- my_oa[
-        my_oa$resource == "best_og_Subcategory",][["pathway"]] %>%
+        my_oa$resource == "Keywords",][["pathway"]] %>%
     unique(.) %>%
+    grep("Signal|Plasmid|Carboxysome|Carbon", ., value = TRUE) %>%
+    c(mandatory_path, .)
+mandatory_path <- my_oa[
+    my_oa$resource == "GOCC Term",][["pathway"]] %>%
+    unique(.) %>%
+    grep("membrane", ., value = TRUE) %>%
     c(mandatory_path, .)
 
 my_oa_filt <- my_oa %>%
@@ -34,13 +40,13 @@ my_oa_filt <- my_oa %>%
     #    "EC level 2 name", "EC level 3 name",
     #    "KEGG_Reaction_Name", "KEGG_rclass_Name",
     #    "KEGG_Module_Name", "KEGG_brite_Name", "best_og_Category")) %>%
-    dplyr::filter(., !is.na(padj) & padj <= 0.1) %>%
+    dplyr::filter(., !is.na(padj) & padj <= 0.05) %>%
     dplyr::group_by(., set) %>%
     dplyr::arrange(., dplyr::desc(score)) %>%
     dplyr::mutate(
         ., Rank = 1:dplyr::n(),
         Selection = dplyr::case_when(
-            Rank %in% c(1:20) ~ "Top",
+            Rank %in% c(1:100) ~ "Top",
             TRUE ~ "")) %>%
     dplyr::ungroup(.) %>%
     dplyr::filter(., pathway %in% mandatory_path | Selection == "Top")
@@ -49,19 +55,22 @@ my_oa_filt <- my_oa %>%
 path_filt <- unique(my_oa_filt$pathway)
 
 my_oa_final <- my_oa_filt %>%
-    dplyr::filter(., pathway %in% path_filt & padj <= 0.5) %>%
+    #dplyr::filter(., pathway %in% path_filt & padj <= 0.5) %>%
+    dplyr::filter(., pathway %in% mandatory_path & padj <= 0.05) %>%
     dplyr::mutate(., padj_range = dplyr::case_when(
         padj <= 0.001 ~ "<= 0.001",
         padj <= 0.01 ~ "<= 0.01",
         padj <= 0.05 ~ "<= 0.05",
-        padj <= 0.1 ~ "<= 0.1",
-        padj <= 0.5 ~ "<= 0.5",
         TRUE ~ NA_character_
-    ))
+    )) %>%
+    dplyr::filter(
+        ., resource %in% c(
+            "Miscellaneous", "best_og_Subcategory",
+            "Characterization", "Keywords", "GOCC Term"))
 
 my_oa_final$padj_range <- factor(
     x = my_oa_final$padj_range,
-    levels = c("<= 0.001", "<= 0.01", "<= 0.05", "<= 0.1", "<= 0.5"),
+    levels = c("<= 0.001", "<= 0.01", "<= 0.05"),
     ordered= TRUE)
 my_oa_final$resource <- factor(
     x = my_oa_final$resource,
@@ -96,18 +105,23 @@ my_plots[["Heatmap_oa_sig"]] <- ggplot(
     facet_grid(rows = vars(resource), scales = "free_y", space = "free_y") +
     scale_alpha_manual(
         values = c(
-            `<= 0.001` = 1, `<= 0.01` = 0.8,
-            `<= 0.05` = 0.5, `<= 0.1` = 0.3,
-            `<= 0.5` = 0.1))
+            `<= 0.001` = 1, `<= 0.01` = 0.7,
+            `<= 0.05` = 0.4)) +
+    scale_fill_manual(values = c("#387eb8", "#d1d2d4", "#e21e25", "#fbaf3f", "#3B3B3B", "#834F96"))
 
 my_oa_final <- my_oa %>%
-    dplyr::filter(., pathway %in% path_filt) %>%
+    #dplyr::filter(., pathway %in% path_filt) %>%
+    dplyr::filter(., pathway %in% mandatory_path) %>%
     dplyr::mutate(., padj_range = dplyr::case_when(
         padj <= 0.001 ~ "<= 0.001",
         padj <= 0.01 ~ "<= 0.01",
         padj <= 0.05 ~ "<= 0.05",
         TRUE ~ "> 0.05"
     )) %>%
+    dplyr::filter(
+        ., resource %in% c(
+            "Miscellaneous", "best_og_Subcategory",
+            "Characterization", "Keywords", "GOCC Term")) %>%
     dplyr::mutate(., Perc_bin = dplyr::case_when(
         overlap_perc == 0 ~ "0",
         overlap_perc <= 20 ~ "20",
@@ -143,10 +157,6 @@ my_oa_final$set <- factor(
         "ps 10 - Synechocystis sp. PCC 6803"),
     ordered = TRUE)
 
-my_fills <- c("#FFFFFF", "#FABFB1", "#FA7F7D", "#F5D4A6", "#EBA74D", "#A60D17") %>%
-    set_names(seq(from = 0, to = 100, by = 20))
-my_fills <- c("#FFFFFF", "#EE4540", "#C72B41", "#800834", "#530332", "#2E122D") %>%
-    set_names(seq(from = 0, to = 100, by = 20))
 my_fills <- c("#FFFFFF", "#FFE6E6", "#FFCCCC", "#FF9999", "#FF8080", "#FF4D4D") %>%
     set_names(seq(from = 0, to = 100, by = 20))
 
