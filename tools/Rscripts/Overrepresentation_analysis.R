62c2bbeb54a89ef597938a57486ef3fe1c9d14b4
nnalpas <nalpas.nicolas@gmail.com>
2022-03-18 14:22:11 +0000

    Allow background to be extracted from foreground and for foreground allow integer or true/false to specify sets

--- tools/Rscripts/Overrepresentation_analysis.R
+++ tools/Rscripts/Overrepresentation_analysis.R
@@ -45,23 +45,23 @@ library(magrittr)
 ### Parameters setting up ------------------------------------------------
 
 opt <- list(
-    annotation = "H:/data/Synechocystis_6frame/Custom_annotation/2021-12-21_Custom_Uniprot_Eggnog_annotations.txt",
-    foreground = "H:/data/Synechocystis_6frame/2022-02-14_iBAQ/Scy004_resuscitation_Scy001_iBAQ_norm_t-test_forOA.txt",
-    background = "H:/data/Synechocystis_6frame/Phylostratigraphy/1148.faa",
-    resource = "Miscellaneous,TU ID,Custom_classification,Active site-note,Binding site-note,Catalytic activity-Reaction,ChEBI,Pathway,Site-note,Keywords,Protein existence,Status,Developmental stage,Induction,Tissue specificity,Subcellular location [CC],Intramembrane,Topological domain-note,Transmembrane-note,Post-translational modification,Modified residue-note,Propeptide-id,Signal peptide-note,Transit peptide,Protein families,Domain [FT]-note,Motif-note,Characterization,EC level 1 name,EC level 2 name,EC level 3 name,GOBP Term,GOCC Term,GOMF Term,Interpro_NAME,Panther Name,Panther Protein class,Panther Pathway,PIRSF name,Prosite,Prosite DE,TIGRFAM label,TIGRFAM product_name,best_og_Category,best_og_Subcategory,GOBP Term.EggNOG,GOCC Term.EggNOG,GOMF Term.EggNOG,EC level 1 name.EggNOG,EC level 2 name.EggNOG,EC level 3 name.EggNOG,KEGG_Pathway_Name,KEGG_Module_Name,KEGG_Reaction_Name,KEGG_rclass_Name,KEGG_brite_Name,pfam_id,pfam_description,pfam_clan_id,pfam_clan_description,CAZy,BiGG_Reaction",
-    gene = "#query_name",
-    idcol = "Proteins",
+    annotation = "/mnt/storage/kxmna01/data/Synechocystis_6frame/2022-03-18_Cross_species_abundance/2022-03-18_Cross_species_Eggnog_annotations.txt",
+    foreground = "/mnt/storage/kxmna01/data/Synechocystis_6frame/2022-03-18_Cross_species_abundance/2022-03-18_Synechocystis_ortholog_comparison_forOA.txt",
+    background = NULL,
+    resource = "best_og_Subcategory",
+    gene = "ID",
+    idcol = "KEGG_ko",
     pval = 1,
     padj = 1,
     minsize = 1,
     maxsize = Inf,
     threads = 1,
-    output = "H:/data/Synechocystis_6frame/2022-02-14_iBAQ")
+    output = ".")
 
 # Check whether inputs parameters were provided
 if (
     is.null(opt$output) | is.null(opt$annotation) |
-    is.null(opt$foreground) | is.null(opt$background)) {
+    is.null(opt$foreground)) {
     
     print_help(opt_parser)
     stop("All arguments must be supplied!")
@@ -95,7 +95,7 @@ if (!is.na(opt$idcol) & !is.null(opt$idcol) & opt$idcol != "") {
         input = opt$foreground, sep = "\t", quote = "",
         header = TRUE, stringsAsFactors = FALSE, colClasses = "character") %>%
         tidyr::pivot_longer(data = ., cols = -as.name(opt$idcol)) %>%
-        dplyr::filter(., !is.na(value) & value == 1) %>%
+        dplyr::filter(., !is.na(value) & (value == 1 | isTRUE(value) | value == "TRUE")) %>%
         dplyr::mutate(., name = as.factor(name))
     my_foreground <- split(
         x = my_ranking[[opt$idcol]], f = my_ranking$name)
@@ -107,8 +107,13 @@ if (!is.na(opt$idcol) & !is.null(opt$idcol) & opt$idcol != "") {
         set_names(basename(opt$foreground))
 }
 
-my_background <- seqinr::read.fasta(
-    file = opt$background, seqtype = "AA", as.string = TRUE)
+if (!is.null(opt$background)) {
+    my_background <- seqinr::read.fasta(
+        file = opt$background, seqtype = "AA", as.string = TRUE)
+} else {
+    my_background <- unique(my_ranking$KEGG_ko) %>%
+        set_names(unique(my_ranking$KEGG_ko))
+}
 
 my_resource <- opt$resource %>%
     base::strsplit(x = ., split = ",", fixed = TRUE) %>%
