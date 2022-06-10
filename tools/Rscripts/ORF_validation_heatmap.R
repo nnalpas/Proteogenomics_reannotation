21d42ab70c30915301fd79ec94f541bbf1ac8d09
nnalpas <nalpas.nicolas@gmail.com>
2022-06-10 14:09:04 +0200

    Draw ORF identification metrics as heatmap by replacing "High quality" with "Fmeasure quality"

--- tools/Rscripts/ORF_validation_heatmap.R
+++ tools/Rscripts/ORF_validation_heatmap.R
@@ -8,27 +8,25 @@ library(magrittr)
 
 my_plots <- list()
 
-my_novel_f <- "/mnt/storage/kxmna01/data/Synechocystis_6frame/2022-03-04_ORF_validation/Venn_ORF_validation.txt"
+my_data_f <- "H:/data/Synechocystis_6frame/2022-03-04_ORF_validation/Venn_ORF_validation_fmeasure.RDS"
+
 feature_cols <- c(
     "Proteins", "Novel_peptide_count", "Novel_sum_MSMS_Count",
     "ORFNoveltyReason", "MainID", "Peptide 2+", "RBS valid",
-    "TU valid", "High quality", "PX valid")
+    "TU valid", "Fmeasure valid", "PX valid", "Novel_min_PEP")
 
-my_novel <- data.table::fread(
-    input = my_novel_f, sep = "\t", quote = "", header = TRUE,
-    stringsAsFactors = FALSE, colClasses = "character")
+my_novel <- readRDS(my_data_f)
 
 feature <- my_novel %>%
     dplyr::select(., tidyselect::all_of(feature_cols)) %>%
     dplyr::mutate(., Reason = dplyr::case_when(
-        ORFNoveltyReason %in% c("Potentially novel", "SAV", "Potential alternate start", "Potential alternate end") ~ ORFNoveltyReason,
-        TRUE ~ "Conflicting annotation"
-    )) %>%
-    dplyr::mutate(., MainID = ifelse(MainID != "", MainID, Proteins)) %>%
-    dplyr::arrange(., dplyr::desc(as.integer(Novel_sum_MSMS_Count)))
+        grepl("match elsewhere", ORFNoveltyReason) ~ "Conflicting annotation",
+        TRUE ~ ORFNoveltyReason
+    ) %>% sub(";.+", "", .)) %>%
+    dplyr::arrange(., dplyr::desc(as.integer(Novel_sum_MSMS_Count)), Novel_min_PEP)
 
-feature$MainID <- factor(
-    x = feature$MainID, levels = feature$MainID, ordered = TRUE)
+feature$Proteins <- factor(
+    x = feature$Proteins, levels = feature$Proteins, ordered = TRUE)
 
 quart_msms <- summary(as.integer(feature$Novel_sum_MSMS_Count))
 feature_format_heat <- feature %>%
@@ -38,30 +36,30 @@ feature_format_heat <- feature %>%
         as.integer(Novel_sum_MSMS_Count) <= quart_msms[["3rd Qu."]] ~ "75%",
         TRUE ~ "100%")) %>%
     dplyr::select(
-        ., MainID, Reason, MSMS_Count, `Peptide 2+`, `TU valid`,
-        `High quality`, `PX valid`) %>%
-    tidyr::pivot_longer(data = ., cols = -MainID)
+        ., Proteins, Reason, MSMS_Count, `Peptide 2+`, `TU valid`,
+        `Fmeasure valid`, `PX valid`) %>%
+    tidyr::pivot_longer(data = ., cols = -Proteins)
 
 feature_format_heat$name <- factor(
     x = feature_format_heat$name,
-    levels = c("Reason", "MSMS_Count", "High quality", "PX valid", "TU valid", "Peptide 2+"),
+    levels = c("Reason", "MSMS_Count", "Fmeasure valid", "PX valid", "TU valid", "Peptide 2+"),
     ordered = TRUE)
 
-feature_format_heat$MainID <- factor(
-    x = feature_format_heat$MainID,
-    levels = feature$MainID,
+feature_format_heat$Proteins <- factor(
+    x = feature_format_heat$Proteins,
+    levels = feature$Proteins,
     ordered = TRUE)
 
 my_cols <- c(
-    `Potential alternate start` = "#387eb8", `Potential alternate end` = "#fbaf3f",
-    `SAV` = "#246E39", `Potentially novel` = "#e21e25",
-    `Conflicting annotation` = "#d1d2d4",
+    `Potential alternate start` = "#d1d2d4", `Potential alternate end` = "#246E39",
+    `SAV` = "#fbaf3f", `Potentially novel` = "#e21e25",
+    `Conflicting annotation` = "#387eb8",
     `TRUE` = "#404040", `FALSE` = "white",
     `25%` = "#3C5941", `50%` = "#B4BD93", `75%` = "#E3C28D", `100%` = "#C7522B")
 
 my_plots[["heat_orf_properties"]] <- ggplot(
     feature_format_heat,
-    aes(x = name, y = MainID, fill = value)) +
+    aes(x = name, y = Proteins, fill = value)) +
     geom_tile(stat = "identity", colour = "white") +
     ggpubr::theme_pubr() +
     theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)) +
