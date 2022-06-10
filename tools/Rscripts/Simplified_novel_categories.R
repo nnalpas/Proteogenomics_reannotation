203e06a963b2b9b41a702a2274f4e9e13822a370
nnalpas <nalpas.nicolas@gmail.com>
2022-06-10 14:07:34 +0200

    Better code to define the conflict in annotation

--- tools/Rscripts/Simplified_novel_categories.R
+++ tools/Rscripts/Simplified_novel_categories.R
@@ -1,26 +1,26 @@
 
 
 
+rm(list = ls())
+
 library(magrittr)
 library(ggplot2)
 
 my_cols <- c("#387eb8", "#d1d2d4", "#e21e25", "#fbaf3f", "#3B3B3B", "#834F96")
 
-my_data_f <- "H:/data/Synechocystis_6frame/2021-12-29_ORF_validation/Venn_ORF_validation.txt"
+my_data_f <- "H:/data/Synechocystis_6frame/2022-03-04_ORF_validation/Venn_ORF_validation_fmeasure.RDS"
 
-my_data <- data.table::fread(
-    input = my_data_f, sep = "\t", quote = "",
-    header = TRUE, stringsAsFactors = FALSE)
+my_data <- readRDS(my_data_f)
 
 my_data_format <- my_data %>%
     dplyr::mutate(., FinalCategory = dplyr::case_when(
-        ORFNoveltyReason %in% c("Potentially novel", "SAV", "Potential alternate start", "Potential alternate start") ~ ORFNoveltyReason,
-        TRUE ~ "Conflicting annotation"
-    ))
+        grepl("match elsewhere", ORFNoveltyReason) ~ "Conflicting annotation",
+        TRUE ~ ORFNoveltyReason
+    ) %>% sub(";.+", "", .))
 
 my_data_format$FinalCategory <- factor(
     x = my_data_format$FinalCategory,
-    levels = unique(my_data_format$FinalCategory),
+    levels = c("Conflicting annotation", "Potential alternate start", "Potentially novel", "SAV"),
     ordered = TRUE)
 
 toplot <- my_data_format %>%
@@ -30,7 +30,7 @@ toplot <- my_data_format %>%
     dplyr::mutate(., Type = "All novel")
 
 toplot <- my_data_format %>%
-    dplyr::filter(., `High quality` == TRUE) %>%
+    dplyr::filter(., `Fmeasure valid` == TRUE) %>%
     plyr::ddply(
         .data = ., .variables = "FinalCategory",
         .fun = dplyr::summarise, Count = dplyr::n(), .drop = FALSE) %>%
