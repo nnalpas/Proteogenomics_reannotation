faff22f4342154af9a455ac5a196648604fddbb0
nnalpas <nalpas.nicolas@gmail.com>
2022-07-21 12:05:52 +0200

    In case no validation with other proteome dataset was done, then write empty columns

--- tools/Rscripts/Include_PX_ORF_validation.R
+++ tools/Rscripts/Include_PX_ORF_validation.R
@@ -1,12 +1,14 @@
 
 
 
+rm(list = ls())
+
 library(magrittr)
 
 opt <- list()
 
-opt$novel <- "H:/data/Synechocystis_6frame/NoveltyExplain/ORF_novelty_reason.RDS"
-opt$novel_valid <- "H:/data/Synechocystis_6frame/Novel_res_validation"
+opt$novel <- "H:/data/Pathogens_6frame/NoveltyExplain/ORF_novelty_reason.RDS"
+opt$novel_valid <- "H:/data/Pathogens_6frame/Novel_res_validation"
 
 orf_reason_final <- readRDS(opt$novel)
 
@@ -15,33 +17,42 @@ my_validation_f <- list.files(
     full.names = TRUE, recursive = TRUE) %>%
     set_names(sub(paste0(opt$novel_valid, "/"), "", dirname(.), fixed = TRUE))
 
-pep_grange <- lapply(my_validation_f, readRDS) %>%
-    as(., "GRangesList") %>%
-    unlist(.)
-
-# Loop through all novel ORFs
-for (x in orf_reason_final$Proteins) {
-    pep_gr_filt <- subset(pep_grange, grepl(paste0(x, "(,|$)"), Proteins)) %>%
-        subset(., group == "Novel")
-    if (length(pep_gr_filt) > 0) {
-        orf_reason_final[orf_reason_final$Proteins == x, "PX_datasets"] <- names(pep_gr_filt) %>%
-            sub("\\..+", "", .) %>%
-            unique(.) %>%
-            paste0(., collapse = ";")
-        orf_reason_final[orf_reason_final$Proteins == x, "PX_novel_sequence"] <- names(pep_gr_filt) %>%
-            sub(".+\\.", "", .) %>%
-            unique(.) %>%
-            paste0(., collapse = ";")
-        orf_reason_final[orf_reason_final$Proteins == x, "PX_novel_peptide_count"] <- names(pep_gr_filt) %>%
-            sub(".+\\.", "", .) %>%
-            dplyr::n_distinct(.)
-        orf_reason_final[orf_reason_final$Proteins == x, "PX_coverage_start"] <- min(
-            c(start(pep_gr_filt), end(pep_gr_filt)))
-        orf_reason_final[orf_reason_final$Proteins == x, "PX_Coverage_end"] <- max(
-            c(start(pep_gr_filt), end(pep_gr_filt)))
+if (length(my_validation_f) > 0) {
+    
+    pep_grange <- lapply(my_validation_f, readRDS) %>%
+        as(., "GRangesList") %>%
+        unlist(.)
+    
+    # Loop through all novel ORFs
+    for (x in orf_reason_final$Proteins) {
+        pep_gr_filt <- subset(pep_grange, grepl(paste0(x, "(,|$)"), Proteins)) %>%
+            subset(., group == "Novel")
+        if (length(pep_gr_filt) > 0) {
+            orf_reason_final[orf_reason_final$Proteins == x, "PX_datasets"] <- names(pep_gr_filt) %>%
+                sub("\\..+", "", .) %>%
+                unique(.) %>%
+                paste0(., collapse = ";")
+            orf_reason_final[orf_reason_final$Proteins == x, "PX_novel_sequence"] <- names(pep_gr_filt) %>%
+                sub(".+\\.", "", .) %>%
+                unique(.) %>%
+                paste0(., collapse = ";")
+            orf_reason_final[orf_reason_final$Proteins == x, "PX_novel_peptide_count"] <- names(pep_gr_filt) %>%
+                sub(".+\\.", "", .) %>%
+                dplyr::n_distinct(.)
+            orf_reason_final[orf_reason_final$Proteins == x, "PX_coverage_start"] <- min(
+                c(start(pep_gr_filt), end(pep_gr_filt)))
+            orf_reason_final[orf_reason_final$Proteins == x, "PX_Coverage_end"] <- max(
+                c(start(pep_gr_filt), end(pep_gr_filt)))
+        }
     }
+    
+} else {
+    orf_reason_final$PX_datasets <- ""
+    orf_reason_final$PX_novel_sequence <- ""
+    orf_reason_final$PX_novel_peptide_count <- ""
+    orf_reason_final$PX_coverage_start <- ""
+    orf_reason_final$PX_Coverage_end <- ""
 }
-
 
 saveRDS(object = orf_reason_final, file = sub(".RDS", "_valid.RDS", opt$novel))
 
