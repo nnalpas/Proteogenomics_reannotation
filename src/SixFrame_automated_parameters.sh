d73548e6ce645a7a3a31c783cc9d36b191a5e72c
nnalpas <nalpas.nicolas@gmail.com>
2022-09-13 11:13:51 +0200

    The MaxQuant processing folder is called MQ_6frame

--- src/SixFrame_automated_parameters.sh
+++ src/SixFrame_automated_parameters.sh
@@ -23,26 +23,26 @@ while IFS= read -r line; do
 	IFS=$'\t' read -r -a array <<< "$line"
 
 	# Create new folders and copy working directory data
-	NEWPARAM=`basename $PARAM | sed "s/.txt/_${array[0]}.txt/"`
-	mkdir -p "$WKDIR/${array[0]}/Genome"; mkdir -p "$WKDIR/${array[0]}/MaxQuant"; mkdir -p "$WKDIR/${array[0]}/Phenodata"
-	cp "$WKDIR/Genome/${array[5]}" "$WKDIR/${array[0]}/Genome/"; cp "$PARAM" "$WKDIR/${array[0]}/Phenodata/$NEWPARAM"
-	find $WKDIR/Genome/ -name "*FIXED.fasta" -exec cp '{}' $WKDIR/${array[0]}/Genome/ \;
+	#NEWPARAM=`basename $PARAM | sed "s/.txt/_${array[0]}.txt/"`
+	#mkdir -p "$WKDIR/${array[0]}/Genome"; mkdir -p "$WKDIR/${array[0]}/MQ_6frame"; mkdir -p "$WKDIR/${array[0]}/Phenodata"
+	#cp "$WKDIR/Genome/${array[5]}" "$WKDIR/${array[0]}/Genome/"; cp "$PARAM" "$WKDIR/${array[0]}/Phenodata/$NEWPARAM"
+	#find $WKDIR/Genome/ -name "*FIXED.fasta" -exec cp '{}' $WKDIR/${array[0]}/Genome/ \;
 
 	# Edit the parameter file
-	sed -i -e "s/PROJECT_REPLACE/${array[0]}/g" "$WKDIR/${array[0]}/Phenodata/$NEWPARAM"
-	sed -i -e "s/GENOME_REPLACE/${array[5]}/g" "$WKDIR/${array[0]}/Phenodata/$NEWPARAM"
+	#sed -i -e "s/PROJECT_REPLACE/${array[0]}/g" "$WKDIR/${array[0]}/Phenodata/$NEWPARAM"
+	#sed -i -e "s/GENOME_REPLACE/${array[5]}/g" "$WKDIR/${array[0]}/Phenodata/$NEWPARAM"
 
 	# Source the variable from the parameter file
 	ProjDir="$WKDIR/${array[0]}"
 	source "$WKDIR/${array[0]}/Phenodata/$NEWPARAM"
 
 	# Edit the mqpar file
-	cp "$WKDIR/MaxQuant/${array[4]}" "$WKDIR/${array[0]}/MaxQuant/"; cp "$WKDIR/MaxQuant/mqpar_posix.xml" "$WKDIR/${array[0]}/MaxQuant/";
+	#cp "$WKDIR/MQ_6frame/${array[4]}" "$WKDIR/${array[0]}/MQ_6frame/"; cp "$WKDIR/MQ_6frame/mqpar_posix.xml" "$WKDIR/${array[0]}/MQ_6frame/";
 	SIXFRAMEPROT=`basename $GENOME | perl -p -e "s%^%${WKDIR}\\/${array[0]}\\/Nuc_translation\\/Find0_%" | perl -p -e "s/\\.fasta/_FIXED.fasta/"`
-	sed -i -e "s%ORF_REPLACE%$SIXFRAMEPROT%g" "$WKDIR/${array[0]}/MaxQuant/mqpar_posix.xml"
-	sed -i -e "s%REF_REPLACE%$UNIREFPROT%g" "$WKDIR/${array[0]}/MaxQuant/mqpar_posix.xml"
-	sed -i -e "s%RAW_REPLACE%$WKDIR\\/${array[0]}\\/MaxQuant\\/${array[4]}%g" "$WKDIR/${array[0]}/MaxQuant/mqpar_posix.xml"
-	sed -i -e "s%PROJECT_REPLACE%${array[0]}%g" "$WKDIR/${array[0]}/MaxQuant/mqpar_posix.xml"
+	#sed -i -e "s%ORF_REPLACE%$SIXFRAMEPROT%g" "$WKDIR/${array[0]}/MQ_6frame/mqpar_posix.xml"
+	#sed -i -e "s%REF_REPLACE%$UNIREFPROT%g" "$WKDIR/${array[0]}/MQ_6frame/mqpar_posix.xml"
+	#sed -i -e "s%RAW_REPLACE%$WKDIR\\/${array[0]}\\/MQ_6frame\\/${array[4]}%g" "$WKDIR/${array[0]}/MQ_6frame/mqpar_posix.xml"
+	#sed -i -e "s%PROJECT_REPLACE%${array[0]}%g" "$WKDIR/${array[0]}/MQ_6frame/mqpar_posix.xml"
 
 	qsub -d ${HOME}/ws/tu_kxmna01-Proteogenomics-0 -m abe -M nicolas.nalpas@ifiz.uni-tuebingen.de -j eo -V -v SCRIPT_FLAGS="$WKDIR/${array[0]}/Phenodata/$NEWPARAM" -q short -l nodes=1:ppn=1,walltime=24:00:00 -N maxquant ${HOME}/bin/SixFrame_proteogenomics.sh
 	
