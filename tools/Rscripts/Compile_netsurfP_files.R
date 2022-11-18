35f1dd4147f3fce9d2bc216d66e2d42197492a70
nnalpas <nalpas.nicolas@gmail.com>
2022-11-18 09:22:58 +0100

    Need to format the protein ID so that all comma are removed (otherwise it poses problem when reading csv) and it also needs to be consistent with the txt file

--- tools/Rscripts/Compile_netsurfP_files.R
+++ tools/Rscripts/Compile_netsurfP_files.R
@@ -1,12 +1,24 @@
 
 
 
+rm(list = ls())
+
 library(magrittr)
 library(ggplot2)
 
-my_netsurf_csv_f <- "H:/data/Synechocystis_6frame/2022-02-23_NetsurfP/Phosphorylated_proteins.csv"
+my_netsurf_csv_f <- "C:/Users/nalpanic/SynologyDrive/Work/Abaumannii_trimeth/Netsurfp/Trimethylated/Trimethylated_proteins.csv"
 
-my_netsurf_txt_f <- "H:/data/Synechocystis_6frame/2022-02-23_NetsurfP/Phosphorylated_proteins_netsurfp.txt"
+my_netsurf_csv <- readLines(con = my_netsurf_csv_f)
+
+my_netsurf_csv %<>%
+    sub(">(p?1?ABYAL[0-9]+)_.+_(,[A-Z],[0-9])", "\\1\\2", .)
+
+writeLines(text = my_netsurf_csv, con = "C:/Users/nalpanic/SynologyDrive/Work/Abaumannii_trimeth/Netsurfp/Trimethylated/Trimethylated_proteins_cleaned.csv")
+
+
+my_netsurf_csv_f <- "C:/Users/nalpanic/SynologyDrive/Work/Abaumannii_trimeth/Netsurfp/Trimethylated/Trimethylated_proteins_cleaned.csv"
+
+my_netsurf_txt_f <- "C:/Users/nalpanic/SynologyDrive/Work/Abaumannii_trimeth/Netsurfp/Trimethylated/Trimethylated_proteins.netsurfp.txt"
 
 my_netsurf_csv <- data.table::fread(
     input = my_netsurf_csv_f, sep = ",",
@@ -14,7 +26,8 @@ my_netsurf_csv <- data.table::fread(
 
 my_netsurf_txt <- data.table::fread(
     input = my_netsurf_txt_f, sep = "\t",
-    header = TRUE, stringsAsFactors = FALSE)
+    header = TRUE, stringsAsFactors = FALSE) %>%
+    dplyr::mutate(., Proteins = sub("(p?1?ABYAL[0-9]+)_.+", "\\1", Proteins))
 
 my_netsurf_txt %<>%
     dplyr::select(
@@ -26,7 +39,7 @@ my_netsurf <- dplyr::left_join(
 
 data.table::fwrite(
     x = my_netsurf,
-    file = "H:/data/Synechocystis_6frame/2022-02-23_NetsurfP/Phosphorylated_proteins_formatted.txt",
+    file = sub(".txt", "_formatted.txt", my_netsurf_txt_f),
     append = FALSE, quote = FALSE, sep = "\t",
     row.names = FALSE, col.names = TRUE)
 
